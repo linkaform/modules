73eea2a839fa8b3a310f79eefdbc68714a313cdf
Linkaform Develop Team <backend@linkaform.com>
2024-04-23 11:36:14 -0600

    actualicacion de modulo de stock

--- stock/items/scripts/Stock/stock_utils.py
+++ stock/items/scripts/Stock/stock_utils.py
@@ -38,7 +38,10 @@ class Stock(Stock):
                 'Setis': 1,
             }
 
+
+        self.CONTACTO_CENTER_FORM_ID = 85639
         self.f.update({
+            'parts_group':'62c5da67f850f35cc2483346',
             })
         self.answer_label = self._labels()
 
@@ -968,7 +971,6 @@ class Stock(Stock):
         skus = self.get_product_sku(search_codes)
         return skus
 
-
     def get_stock_info_form_answer(self, answers={}, data={}):
         if not answers:
             answers = self.answers
@@ -1529,8 +1531,9 @@ class Stock(Stock):
     #             folios_2_update.append(record.get('folio'))
     #     return res
 
+
     def move_in(self):
-        # answers = self._labels()
+        answers = self._labels()
         print('-----------------------------answers', self.answer_label)
         warehouse = self.answer_label['warehouse']
         location = self.answer_label['warehouse_location']
@@ -1641,6 +1644,8 @@ class Stock(Stock):
             ###################################
 
             moves[self.f['move_dest_folio']] = stock['folio']
+            moves[self.f['inv_adjust_grp_status']] = 'done'
+            
             set_location = f"{product_code}_{sku}_{lot_number}_{warehouse_to}_{location_to}"
             if set_location in move_locations:
                 msg = "You are trying to move the same lot_number: {lot_number} twice from the same location. Please check"
@@ -2240,6 +2245,109 @@ class Stock(Stock):
             result = result.get(product_code,0)
         # print('SELECCION DE PLANTA', result)
         return result  
+
+    def stock_one_many_one(self, move_type, product_code=None, sku=None, lot_number=None, warehouse=None, location=None, date_from=None, date_to=None, status='done', **kwargs):
+        print('11move_type=',move_type)
+        res = super().stock_one_many_one(
+            move_type, 
+            product_code=product_code, 
+            sku=sku, 
+            lot_number=lot_number, 
+            warehouse=warehouse, 
+            location=location, 
+            date_from=date_from, 
+            date_to=date_to, 
+            status=status, 
+            **kwargs)
+        if move_type == 'out':
+            res += self.stock_out_ordens_de_servicio(product_code, sku, lot_number, warehouse, location, date_from, date_to, status,  **kwargs)
+        return res
+
+    def stock_out_ordens_de_servicio(self, product_code=None, sku=None, lot_number=None, warehouse=None, location=None, date_from=None, date_to=None, status='done', **kwargs):
+        match_query = {
+            "deleted_at":{"$exists":False},
+            }
+        form_query = {"form_id": self.CONTACTO_CENTER_FORM_ID}
+        match_query.update(form_query)
+        if date_from or date_to:
+            match_query.update(self.get_date_query(date_from=date_from, date_to=date_to, date_field_id=self.f['grading_date']))
+        unwind = {'$unwind': '$answers.{}'.format(self.f['parts_group'])}
+        unwind_query = {}
+        unwind_stage = []
+        print('warehouse', warehouse)
+        print('location', location)
+        print('product_code', product_code)
+        print('sku', sku)
+        print('lot_number', lot_number)
+        inc_folio = kwargs.get("inc_folio")
+        nin_folio = kwargs.get("nin_folio")
+        if inc_folio:
+            match_query.update({"folio":inc_folio})
+        if nin_folio:
+            match_query.update({"folio": {"$ne":nin_folio }})
+
+        if warehouse:
+            match_query.update({f"answers.{self.WAREHOUSE_OBJ_ID}.{self.f['warehouse']}":warehouse})
+        if location:
+            unwind_query.update({f"answers.{self.f['parts_group']}.{self.STOCK_INVENTORY_OBJ_ID}.{self.f['warehouse_location']}":location})
+       
+        if product_code:
+            unwind_query.update(
+                {f"answers.{self.f['parts_group']}.{self.STOCK_INVENTORY_OBJ_ID}.{self.f['product_code']}":product_code}
+                )
+        if sku:
+            unwind_query.update(
+                {f"answers.{self.f['parts_group']}.{self.STOCK_INVENTORY_OBJ_ID}.{self.f['sku']}":sku}
+                )
+        if lot_number:
+            unwind_query.update(
+                {f"answers.{self.f['parts_group']}.{self.STOCK_INVENTORY_OBJ_ID}.{self.f['lot_number']}":lot_number},
+                )
+        # if status:
+        #     match_query.update({f"answers.{self.f['stock_status']}":status})
+        # if status:
+        #     unwind_query.update({f"answers.{self.f['parts_group']}.{self.f['inv_adjust_grp_status']}":status})
+        if date_from or date_to:
+            match_query.update(self.get_date_query(date_from=date_from, date_to=date_to, date_field_id=self.f['grading_date']))
+        project = {'$project':
+                {'_id': 1,
+                    'product_code':f"$answers.{self.f['parts_group']}.{self.STOCK_INVENTORY_OBJ_ID}.{self.f['product_code']}",
+                    'total': f"$answers.{self.f['parts_group']}.{self.f['move_group_qty']}",
+                    }
+            }
+        query= [{'$match': match_query }]
+        query.append(unwind)
+        if unwind_query:
+            query.append({'$match': unwind_query })
+        if unwind_stage:
+            query += unwind_stage
+        query.append(project)
+        query += [
+            {'$group':
+                {'_id':
+                    { 'product_code': '$product_code',
+                      },
+                  'total': {'$sum': '$total'},
+                  }
+            },
+            {'$project':
+                {'_id': 0,
+                'product_code': '$_id.product_code',
+                'total': '$total',
+                }
+            },
+            {'$sort': {'product_code': 1}}
+            ]
+        res = self.cr.aggregate(query)
+        result = {}
+        # print('query=', simplejson.dumps(query, indent=3))
+        for r in res:
+            pcode = r.get('product_code')
+            result[pcode] = result.get(pcode, 0)        
+            result[pcode] += r.get('total',0)
+        if product_code:
+            result = result.get(product_code,0)
+        return result
 
     def stock_supplier(self, date_from=None, date_to=None, product_code=None, warehouse=None, location=None, lot_number=None,  status='posted', **kwargs):
         match_query = {
