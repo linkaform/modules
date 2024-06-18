d7d4915e7ba26711bbeb9156a2661c6a27087608
Linkaform Develop Team <backend@linkaform.com>
2024-06-17 18:05:34 -0600

    se agrega cache a movimeinto de stock

--- stock/items/scripts/Stock/contact_center.py
+++ stock/items/scripts/Stock/contact_center.py
@@ -18,9 +18,23 @@ class Stock(Stock):
             lot_number = part.get(self.STOCK_INVENTORY_OBJ_ID,{}).get(self.f['lot_number'])
             location = part.get(self.STOCK_INVENTORY_OBJ_ID,{}).get(self.f['warehouse_location'])
             print('product_code', product_code)
+            print('sku', sku)
+            print('lot_number', lot_number)
+            qty_to_move = part.get(self.f['move_group_qty'])
+            print('qty_to_move', qty_to_move)
             part_inv = self.get_invtory_record_by_product(self.FORM_INVENTORY_ID, product_code, sku, lot_number, warehouse, location)
             print('part_inv', part_inv)
-            if part_inv.get('folio'):
+            if part_inv and part_inv.get('folio'):
+                self.cache_set({
+                    '_id': f'{product_code}_{sku}_{lot_number}_{warehouse}_{location}',
+                    'move_out':qty_to_move,
+                    'lot_number':lot_number,
+                    'sku':sku,
+                    'product_code':product_code,
+                    'warehouse': warehouse,
+                    'warehouse_location': location,
+                    'record_id': self.record_id
+                })
                 self.update_stock(answers={}, form_id=self.FORM_INVENTORY_ID, folios= part_inv.get('folio'))
 
 
