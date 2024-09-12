df83e241d472f52dfcb7adcd9caf0d9f0dc57b1d
Maricruz Alvarado <maricruzalvaradoc@outlook.com>
2024-09-12 15:52:20 -0600

    articulos perdidos catalogo

--- accesos/items/scripts/Accesos/articulos_perdidos.py
+++ accesos/items/scripts/Accesos/articulos_perdidos.py
@@ -15,19 +15,19 @@ if __name__ == "__main__":
     option = data.get("option",'')
 
     data_article = data.get("data_article",{
-        'status_perdido':'pendiente',
+        'estatus_perdido':'pendiente',
         'date_hallazgo_perdido':'2024-07-08 19:43:01',
         'ubicacion_perdido':'Planta Monterrey',
         'area_perdido':'Recursos eléctricos',
         'articulo_perdido':'Suéter',
-        'photo_perdido':[{
+        'foto_perdido':[{
             'file_url':'https://f001.backblazeb2.com/file/app-linkaform/public-client-126/71202/60b81349bde5588acca320e1/65dd1061092cd19498857933.jpg',
             'file_name':'ejemploidentificacion.jpg',
         }],
-        'comments_perdido':'Se perdio Suetercito',
+        'comentario_perdido':'Se perdio Suetercito',
         'guard_perdido':'Pedro Cervantes',
         'recibe_perdido':'Karla Perez',
-        'phone_recibe_perdido':'1234567891',
+        'telefono_recibe_perdido':'1234567891',
         'identification_perdido':[],
         'date_entrega_perdido':'',
     })
@@ -44,7 +44,7 @@ if __name__ == "__main__":
     #option = 'get_articles';
     #option = 'update_article';
     #option = 'delete_article';
-    if option == 'new_article':
+    if option == 'nuevo_articulo':
         response = acceso_obj.create_article_lost(data_article)
     elif option == 'get_articles':
         response = acceso_obj.get_list_article_lost(location, area)
@@ -52,6 +52,8 @@ if __name__ == "__main__":
         response = acceso_obj.update_article_lost(data_article_update, folio)
     elif option == 'delete_article':
         response = acceso_obj.delete_article_lost(folio)
+    elif option == 'catalogo_tipo_articulo':
+        response = acceso_obj.catalogo_tipo_articulo()
     else :
         response = {"msg": "Empty"}
     acceso_obj.HttpResponse({"data":response})
