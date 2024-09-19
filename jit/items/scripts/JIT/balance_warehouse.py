81422c99bfba825a33a96b23dbf8e7fc9aa1985f
Linkaform Develop Team <backend@linkaform.com>
2024-09-19 11:08:47 -0600

    se agrega moduo de jit y de carga univeral

---
+++ jit/items/scripts/JIT/balance_warehouse.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import sys, simplejson, copy
+
+from jit_utils import JIT
+
+from account_settings import *
+
+
+if __name__ == '__main__':
+    jit_obj = JIT(settings, sys_argv=sys.argv, use_api=True)
+    jit_obj.console_run()
+
+    response = jit_obj.balance_warehouse()
+    print(stop_balance)
+    print('TODO: revisar si un create no estuvo bien y ponerlo en error o algo')
+    jit_obj.answers[jit_obj.f['status']] =  'done'
+    print('asi termina', stock_obj.answers)
+    sys.stdout.write(simplejson.dumps({
+        'status': 101,
+        'replace_ans': jit_obj.answers,
+        }))
