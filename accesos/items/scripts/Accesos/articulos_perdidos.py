1f9bb988d6c9813b7eebde13e3f70eac9c2736a4
Maricruz Alvarado <maricruzalvaradoc@outlook.com>
2024-09-19 10:37:13 -0600

    cambios articulos perdidos

--- accesos/items/scripts/Accesos/articulos_perdidos.py
+++ accesos/items/scripts/Accesos/articulos_perdidos.py
@@ -15,27 +15,33 @@ if __name__ == "__main__":
     option = data.get("option",'')
 
     data_article = data.get("data_article",{
-        'estatus_perdido':'pendiente',
-        'date_hallazgo_perdido':'2024-07-08 19:43:01',
-        'ubicacion_perdido':'Planta Monterrey',
-        'area_perdido':'Recursos eléctricos',
-        'articulo_perdido':'Suéter',
-        'foto_perdido':[{
-            'file_url':'https://f001.backblazeb2.com/file/app-linkaform/public-client-126/71202/60b81349bde5588acca320e1/65dd1061092cd19498857933.jpg',
-            'file_name':'ejemploidentificacion.jpg',
-        }],
-        'comentario_perdido':'Se perdio Suetercito',
         'guard_perdido':'Pedro Cervantes',
-        'recibe_perdido':'Karla Perez',
-        'telefono_recibe_perdido':'1234567891',
-        'identification_perdido':[],
-        'date_entrega_perdido':'',
+        'estatus_perdido':'pendiente',
+        'foto_perdido': [{
+            'file_url':'https://f001.backblazeb2.com/file/app-linkaform/public-client-126/71202/60b81349bde5588acca320e1/65dd1061092cd19498857933.jpg',
+            'file_name':'ejemploidentificacion.jpg'
+        }],
+        'date_hallazgo_perdido':'2024-07-08 19:43:01',
+        'ubicacion_perdido':"Planta Monterrey",
+        'area_perdido':'Recursos eléctricos',
+        'comentario_perdido':"soy un comentario",
+        'tipo_articulo_perdido': 'Recursos eléctricos',
+        'articulo_seleccion':"Dron",
+        'articulo_perdido':"",
+        'color_perdido':"Blanco",
+        'descripcion':"dron blanco",
+        'guard_perdido':"",
+        'quien_entrega':"Interno",
+        'quien_entrega_interno':"",
+        'quien_entrega_externo':"nombre de persona",
+        'locker_perdido':'L2'
     })
     data_article_update = data.get("data_article_update",{
         'status_perdido':'entregado',
-        'date_entrega_perdido':'2024-07-09 19:43:01',
+        'date_entrega_perdido':'2024-07-09 19:43:01'
     })
     location = data.get("location")
+    status = data.get("status","")
     area = data.get("area")
     folio = data.get("folio")
     tipo = data.get("tipo","")
@@ -47,7 +53,7 @@ if __name__ == "__main__":
     if option == 'nuevo_articulo':
         response = acceso_obj.create_article_lost(data_article)
     elif option == 'get_articles':
-        response = acceso_obj.get_list_article_lost(location, area)
+        response = acceso_obj.get_list_article_lost(location, area,status)
     elif option == 'update_article':
         response = acceso_obj.update_article_lost(data_article_update, folio)
     elif option == 'delete_article':
@@ -57,6 +63,8 @@ if __name__ == "__main__":
             response = acceso_obj.catalogo_tipo_articulo(tipo)
         else:
             response = acceso_obj.catalogo_tipo_articulo()
+    elif option == 'catalogo_area_empleado':
+        response = acceso_obj.catalogo_config_area_empleado()
     else :
         response = {"msg": "Empty"}
     acceso_obj.HttpResponse({"data":response})
