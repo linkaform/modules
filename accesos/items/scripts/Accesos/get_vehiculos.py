c31be6b7ec16c42e10538c18f0a2025fe7d05cf9
Linkaform Develop Team <backend@linkaform.com>
2024-10-10 10:22:28 -0600

    se creo script para obgtener veihuclos y url de area de acceso

---
+++ accesos/items/scripts/Accesos/get_vehiculos.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+import sys, simplejson
+from linkaform_api import settings
+from account_settings import *
+
+from app import Accesos
+
+class Accesos(Accesos):
+    pass
+    
+if __name__ == "__main__":
+    acceso_obj = Accesos(settings, sys_argv=sys.argv)
+    acceso_obj.console_run()
+    #-FILTROS
+    data = acceso_obj.data.get('data',{})
+    marca = data.get('marca',"")
+    tipo = data.get('tipo',"")
+
+    #-FUNCTIONS
+    if tipo and marca:
+        response = acceso_obj.vehiculo_modelo(tipo, marca)
+    elif tipo:
+        response = acceso_obj.vehiculo_marca(tipo)
+    else:
+        response = acceso_obj.vehiculo_tipo()
+    print('================ END RETURN =================')
+    print(simplejson.dumps(response, indent=3))
+    acceso_obj.HttpResponse({"data":response})
+
