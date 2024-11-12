8b98471bfcdae8ca611e3482971f2c983c5b39da
paco soto <paco@linkaform.com>
2024-11-12 17:06:46 -0600

    se agrego carpeta para reportes de Accesos y reporte pases

---
+++ accesos/items/reports/Accesos_Reports/pass_report.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import sys, simplejson, math
+import json
+import math
+from datetime import timedelta, datetime
+
+from linkaform_api import settings, base
+from account_settings import *
+
+print('inicia....')
+#Se agrega path para que obtenga el archivo de Stock de este modulo
+sys.path.append('/srv/scripts/addons/modules/accesos/items/scripts/Accesos')
+from accesos_utils import Accesos
+
+if __name__ == "__main__":
+    script_obj = base.LKF_Base(settings, sys_argv=sys.argv, use_api=True)
+    acc_obj = Accesos(settings, sys_argv=sys.argv, use_api=True)
+    acc_obj.console_run()
+
+    data = acc_obj.data
+    data = data.get('data',[])
+    location = data.get('location', 'Planta Monterrey')
+    
+    option = data.get('option','get_report')
+
+    if option == 'get_report':
+        records = acc_obj.get_cantidades_de_pases()
+        # records = acc_obj.get_cantidades_de_pases(x_empresa=True)
+        # records = acc_obj.get_cantidades_de_pases_x_persona(contratista='Empresa de Limpieza SA de CV')
+        # todos_los_pases = acc_obj.format_cantidades_de_pases(records)
+        # todos_los_pases_x_contratista = acc_obj.get_pases_x_contratista('Pelotas Dragón',records)
+        # todos_los_pases_x_perfil = acc_obj.get_pases_x_perfil(records)
+        # todos_los_pases_x_persona = acc_obj.get_pases_x_persona(records)
+
+        # print('records////////', records)
+        # print('lista de pases//////////', todos_los_pases)
+        # print('lista de pases x contratista/////////', todos_los_pases_x_contratista)
+        # print('lista de pases x perfil/////////', todos_los_pases_x_perfil)
+        # print('lista de pases x persona/////////', todos_los_pases_x_persona)
+        
+        print('recordssss devueltos/////////', records)
+        script_obj.HttpResponse({
+            "cantidad_de_pases": records,
+        })
+
+
+    elif option == 'get_catalog':
+        pass
+        # script_obj.HttpResponse({
+        #     "" : ""
+        # })
