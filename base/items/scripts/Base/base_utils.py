707c1abd174b8ff1bc921d0bb9264587225d9007
Linkaform Develop Team <backend@linkaform.com>
2024-12-16 11:33:46 -0600

    se cambio create qr para que apunte a bsae y se crea base_utils

---
+++ base/items/scripts/Base/base_utils.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from linkaform_api import base
+from lkf_addons.addons.base.app import Base
+
+
+
+class Base(Base):
+    print('Entra a acceos utils')
+
+    def __init__(self, settings, sys_argv=None, use_api=False):
+        super().__init__(settings, sys_argv=sys_argv, use_api=use_api)
+
