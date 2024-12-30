033347cab8899e3c35fc67ebdcf6b18f5ee3791d
Jose Pato V <josepato@linkaform.com>
2024-12-29 23:58:52 -0600

    se agregaron reportes de powerbi

---
+++ stock_greenhouse/items/reports/GreenhouseReports/powerbi_inventory_s4.py
@@ -0,0 +1,52 @@
+#-*- coding: utf-8 -*-
+
+import simplejson, sys
+
+from gh_stock_report import Reports
+
+from account_settings import *
+
+class Reports(Reports):
+
+
+    def get_query(self):
+        query = {
+            "collection":"form_answer",
+            "db_name":"infosync_answers_client_9908",
+            "query" :[
+                    {"$match":{
+                        "form_id":  self.FORM_INVENTORY_ID,
+                        f"answers.{self.f['inventory_status']}":  'active',
+                        "deleted_at":{"$exists":False},
+                        }
+                    },
+                    {"$project":{
+                        "_id":1,
+                        'plant_code': f"$answers.{self.SKU_OBJ_ID}.{self.f['product_code']}",
+                        'ready_year_week': f"$answers.{self.f['product_lot']}",
+                        'actuals': f"$answers.{self.f['actuals']}",
+                        #'actual_eaches': f"$answers.{self.f['actual_eaches_on_hand']}",
+                        }
+                    },
+                    ],
+            "command": "aggregate",
+            "selectColumns":[],
+            "input_schema":  [
+              { "plant_code": "text" },
+              { "ready_year_week": "integer" },
+              { "actuals": "integer" },
+            ],
+            "output_schema":
+            [
+              { "plant_code": "text" },
+              { "ready_year_week": "integer" },
+              { "actuals": "integer" },
+            ]
+        }
+        return query
+
+if __name__ == "__main__":
+    report_obj = Reports(settings, sys_argv=sys.argv, use_api=True)
+    report_obj.console_run()
+    query = report_obj.get_query()
+    print(simplejson.dumps(query))
--- stock_greenhouse/items/reports/__init__.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-from lkf_addons.items.report_resource import ReportResource
-
-
-install_order = []
