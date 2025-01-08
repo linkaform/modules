1a275beea456890f0adc3fc6594c971da6e13fe9
paco soto <francisotomtz@gmail.com>
2025-01-08 17:32:37 -0600

    se corrige error que no detectaba accesos_utils en script update_status_pass y se ajusta la respuesta del script

--- accesos/items/scripts/Accesos/Rutinas/update_status_pass.py
+++ accesos/items/scripts/Accesos/Rutinas/update_status_pass.py
@@ -3,6 +3,7 @@ import sys, simplejson
 from linkaform_api import settings
 from account_settings import *
 
+sys.path.append('/srv/scripts/addons/modules/accesos/items/scripts/Accesos')
 from accesos_utils import Accesos
 
 class Accesos(Accesos):
@@ -15,9 +16,10 @@ if __name__ == "__main__":
     acceso_obj = Accesos(settings, sys_argv=sys.argv, use_api=True)
     acceso_obj.console_run()
 
-    acceso_obj.update_pass_status()
+    res = acceso_obj.update_pass_status()
     sys.stdout.write(simplejson.dumps({
         'status': 101,
-        'replace_ans': acceso_obj.answers
+        'replace_ans': acceso_obj.answers,
+        'matched_documents': res
     }))
     
