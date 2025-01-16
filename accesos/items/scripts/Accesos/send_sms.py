d38795f965febffa040137eb84ccc857ee33b638
paco soto <francisotomtz@gmail.com>
2025-01-15 18:33:47 -0600

    Modificacion en script send_sms no se convertia en booleano correctamente un dato

--- accesos/items/scripts/Accesos/send_sms.py
+++ accesos/items/scripts/Accesos/send_sms.py
@@ -18,12 +18,20 @@ if __name__ == "__main__":
 
     #Saber si sera un pre_sms o no
     pre_sms_raw = json.loads(sys.argv[2])
-    pre_sms = pre_sms_raw.get('pre_sms', '').lower() == 'true'
+    pre_sms_value = pre_sms_raw.get('pre_sms', '')
+
+    # Convertir a booleano de forma segura
+    if isinstance(pre_sms_value, bool):
+        pre_sms = pre_sms_value
+    else:
+        pre_sms = pre_sms_value.lower() == 'true'
 
     #Datos necesario para enviar el sms
     telefono_invitado = acceso_obj.answers.get('662c2937108836dec6d92582', '')
     nombre_invitado = acceso_obj.answers.get('662c2937108836dec6d92580', '')
     link_completar_pase = acceso_obj.answers.get('6732aa1189fc6b0ae27e3824', '')
+    # nombre_visita_a = acceso_obj.answers.get('663d4ba61b14fab90559ebb0', '')[0].get('677ffe8c638c8536ff37effb', '').get('62c5ff407febce07043024dd', '')
+    # ubicacion = acceso_obj.answers.get('666718cb1bdffb6d8fc908d6', '').get('663e5c57f5b8a7ce8211ed0b', '')
     nombre_visita_a = acceso_obj.answers.get('663d4ba61b14fab90559ebb0', '')[0].get('66a83ab5ca3453e21ea08d19', '').get('62c5ff407febce07043024dd', '')
     ubicacion = acceso_obj.answers.get('66a83a74de752e12018fbc3c', '').get('663e5c57f5b8a7ce8211ed0b', '')
     fecha_desde = acceso_obj.answers.get('662c304fad7432d296d92582', '')
