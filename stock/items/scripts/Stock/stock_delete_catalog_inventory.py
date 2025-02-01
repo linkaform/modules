5f5088eb5e5cf48880723ffcf2daede81d2cf2c3
Linkaform Develop Team <backend@linkaform.com>
2025-02-01 15:46:39 -0600

    se verifica inventario antes de borrar, si existe el registro se utiliza funcion de caja

--- stock/items/scripts/Stock/stock_delete_catalog_inventory.py
+++ stock/items/scripts/Stock/stock_delete_catalog_inventory.py
@@ -9,9 +9,11 @@ if __name__ == '__main__':
     stock_obj = Stock(settings, sys_argv=sys.argv)
     stock_obj.console_run()
     #current record
-    record_catalog = stock_obj.get_record_catalog_del()
-    print('catalog record to delete', record_catalog)
-    stock_obj.del_catalog_record(record_catalog, stock_obj.form_id)
+    product_code, sku, lot_number, warehouse, location = stock_obj.get_product_lot_location()
+    stock = stock_obj.get_product_stock(product_code, sku, lot_number, warehouse, location)
+    if stock.get('actuals') == 0 or stock.get('actuals') < 0:
+        record_catalog = stock_obj.get_record_catalog_del()
+        stock_obj.del_catalog_record(record_catalog, stock_obj.form_id)
     #doing cleanup
     #for done records that for some reason are not deleted
     # record_catalog = get_record_catalog(form_id)
