3e622297e33c97ae56501bfd2de184f125ea4d7c
paco soto <francisotomtz@gmail.com>
2025-02-07 11:55:07 -0600

    se modifico script sync_conf_area_empleados a un set que tiene area y location para sincornizar los catalogos de manera correcta

--- accesos/items/scripts/Accesos/sync_conf_area_empleados.py
+++ accesos/items/scripts/Accesos/sync_conf_area_empleados.py
@@ -40,31 +40,43 @@ class Accesos(Accesos):
             row_catalog = self.lkf_api.search_catalog(self.CONF_AREA_EMPLEADOS_CAT_ID, mango_query)
             # print(f"Resultado de la búsqueda: {row_catalog}")
             if row_catalog:
-                existing_areas = set()
-                existing_locations = set()
-                
+                print('=====Ya hay algunos registros en el catalogo Conf Areas y Empleados======')
+                existing_pairs = set()
+
                 for item_catalog in row_catalog:
-                    area = item_catalog.get(self.mf['nombre_area'])
-                    location = item_catalog.get(self.mf['ubicacion'])
+                    area = item_catalog.get(self.mf['nombre_area'], '').strip()
+                    location = item_catalog.get(self.mf['ubicacion'], '').strip()
 
-                    if area: 
-                        existing_areas.add(area)
-                    if location:
-                        existing_locations.add(location)
+                    if area and location:
+                        existing_pairs.add((area, location))
+                    elif not area and location:
+                        existing_pairs.add(('', location))
 
+                # print('existing_pairssssssss', existing_pairs)
                 for item in grupo_repetitivo:
-                    area_grupo = item.get(self.mf['catalogo_ubicaciones'], {}).get(self.mf['nombre_area'])
-                    location_grupo = item.get(self.mf['catalogo_ubicaciones'], {}).get(self.mf['ubicacion'])
+                    area_grupo = item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.mf['nombre_area'], '').strip()
+                    location_grupo = item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.mf['ubicacion'], '').strip()
+
+                    item[self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID][self.mf['nombre_area']] = area_grupo
+
                     print(f'Area: {area_grupo}, Ubicación: {location_grupo}')
 
-                    if area_grupo in existing_areas:
-                        item.get(self.mf['catalogo_ubicaciones']).update({'existente': True})
+                    if (area_grupo, location_grupo) in existing_pairs:
+                        item[self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID].update({'existente': True})
                     else:
-                        item.get(self.mf['catalogo_ubicaciones']).update({'existente': False})
+                        item[self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID].update({'existente': False})
 
                 # print(grupo_repetitivo)
                 self.format_group_to_catalog(data, grupo_repetitivo)
             else:
+                print('=====No hay ninguno de estos registros en el catalogo Conf Areas y Empleados======')
+                for item in grupo_repetitivo:
+                    area_grupo = item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.mf['nombre_area'], '').strip()
+                    location_grupo = item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.mf['ubicacion']).strip()
+                    
+                    item[self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID][self.mf['nombre_area']] = area_grupo
+
+                    print(f'Area: {area_grupo}, Ubicación: {location_grupo}')
                 # print(grupo_repetitivo)
                 self.format_group_to_catalog(data, grupo_repetitivo)
         except Exception as e:
@@ -78,19 +90,20 @@ class Accesos(Accesos):
         nombre_completo = data.get(self.EMPLOYEE_OBJ_ID, {}).get(self.mf['nombre_empleado'])
         # grupo_repetitivo = data.get(self.mf['areas_grupo'], [])
         usuario_data = data.get(self.EMPLOYEE_OBJ_ID, {})
-
-        for item in usuario_data:
-            answer.update({item: usuario_data[item]})
+        for item, value in usuario_data.items():
+            if isinstance(value, list) and value:
+                if isinstance(value[0], list):
+                    value = value[0]
+            answer[item] = value
         
         answer[self.mf['nombre_empleado']] = nombre_completo
-        answer.pop(self.employee_fields['estatus_disponibilidad'])
-
+        # answer.pop(self.employee_fields['estatus_disponibilidad'])
         for item in grupo_repetitivo:
-            if not item.get(self.mf['catalogo_ubicaciones'], {}).get('existente'):
+            if not item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get('existente'):
                 answer.update(
                     {
-                        self.mf['ubicacion']: item.get(self.mf['catalogo_ubicaciones'], {}).get(self.mf['ubicacion']),
-                        self.mf['nombre_area']: item.get(self.mf['catalogo_ubicaciones'], {}).get(self.mf['nombre_area'])
+                        self.mf['ubicacion']: item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.mf['ubicacion']),
+                        self.mf['nombre_area']: item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.mf['nombre_area'])
                     }
                 )
                 catalogo_metadata.update({'answers': answer})
@@ -128,31 +141,45 @@ class Accesos(Accesos):
             row_catalog = self.lkf_api.search_catalog(self.CONF_AREA_EMPLEADOS_AP_CAT_ID, mango_query)
             # print(f"Resultado de la búsqueda: {row_catalog}")
             if row_catalog:
-                existing_areas = set()
-                existing_locations = set()
-                
-                for item_catalog in row_catalog:
-                    area = item_catalog.get(self.mf['nombre_area_salida'])
-                    location = item_catalog.get(self.mf['ubicacion'])
+                print('=====Ya hay algunos registros en el catalogo Conf Areas y Empleados Apoyo======')
+                existing_pairs = set()
 
-                    if area: 
-                        existing_areas.add(area)
-                    if location:
-                        existing_locations.add(location)
+                for item_catalog in row_catalog:
+                    area = item_catalog.get(self.mf['nombre_area_salida'], '').strip()
+                    location = item_catalog.get(self.mf['ubicacion'], '').strip()
+
+                    if area and location:
+                        existing_pairs.add((area, location))
+                    elif not area and location:
+                        existing_pairs.add(('', location))
+
+                # print('existing_pairssssssss', existing_pairs)
 
                 for item in grupo_repetitivo:
-                    area_grupo = item.get(self.mf['catalogo_ubicaciones'], {}).get(self.mf['nombre_area'])
-                    location_grupo = item.get(self.mf['catalogo_ubicaciones'], {}).get(self.mf['ubicacion'])
+                    area_grupo = item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.mf['nombre_area']).strip()
+                    location_grupo = item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.mf['ubicacion']).strip()
+
+                    item[self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID][self.mf['nombre_area']] = area_grupo
+
                     print(f'Area: {area_grupo}, Ubicación: {location_grupo}')
 
-                    if area_grupo in existing_areas:
-                        item.get(self.mf['catalogo_ubicaciones']).update({'existente': True})
+                    if (area_grupo, location_grupo) in existing_pairs:
+                        item[self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID].update({'existente': True})
                     else:
-                        item.get(self.mf['catalogo_ubicaciones']).update({'existente': False})
+                        item[self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID].update({'existente': False})
 
                 # print(grupo_repetitivo)
                 self.format_group_to_catalog_apoyo(data, grupo_repetitivo)
             else:
+                print('=====No hay ninguno de estos registros en el catalogo Conf Areas y Empleados Apoyo======')
+                for item in grupo_repetitivo:
+                    area_grupo = item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.mf['nombre_area'], '').strip()
+                    location_grupo = item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.mf['ubicacion']).strip()
+                    
+                    item[self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID][self.mf['nombre_area']] = area_grupo
+
+                    print(f'Area: {area_grupo}, Ubicación: {location_grupo}')
+                    item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID).pop('existente', None)
                 # print(grupo_repetitivo)
                 self.format_group_to_catalog_apoyo(data, grupo_repetitivo)
         except Exception as e:
@@ -167,18 +194,21 @@ class Accesos(Accesos):
         # grupo_repetitivo = data.get(self.mf['areas_grupo'], [])
         usuario_data = data.get(self.EMPLOYEE_OBJ_ID, {})
 
-        for item in usuario_data:
-            answer.update({item: usuario_data[item]})
+        for item, value in usuario_data.items():
+            if isinstance(value, list) and value:
+                if isinstance(value[0], list):
+                    value = value[0]
+            answer[item] = value
         
         answer[self.mf['nombre_guardia_apoyo']] = nombre_completo
-        answer.pop(self.employee_fields['estatus_disponibilidad'])
+        # answer.pop(self.employee_fields['estatus_disponibilidad'])
 
         for item in grupo_repetitivo:
-            if not item.get(self.mf['catalogo_ubicaciones'], {}).get('existente'):
+            if not item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get('existente'):
                 answer.update(
                     {
-                        self.mf['ubicacion']: item.get(self.mf['catalogo_ubicaciones'], {}).get(self.mf['ubicacion']),
-                        self.mf['nombre_area_salida']: item.get(self.mf['catalogo_ubicaciones'], {}).get(self.mf['nombre_area'])
+                        self.mf['ubicacion']: item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.mf['ubicacion']),
+                        self.mf['nombre_area_salida']: item.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.mf['nombre_area'])
                     }
                 )
                 catalogo_metadata.update({'answers': answer})
@@ -186,7 +216,7 @@ class Accesos(Accesos):
                 self.lkf_api.post_catalog_answers(catalogo_metadata, jwt_settings_key='APIKEY_JWT_KEY')
                 # print('Respuesta de la creación del registro en el catálogo:', res)
             else:
-                print("Ya se encuentran registradas esas areas y ubicaciones")
+                print("Ya se encuentran registradas esas areas y ubicaciones en el catálogo de apoyo")
 
 if __name__ == "__main__":
     acceso_obj = Accesos(settings, sys_argv=sys.argv)
