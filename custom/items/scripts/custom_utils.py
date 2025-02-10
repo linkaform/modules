fa2d6f2684d476d69a9c6be92863339cb1300ef3
LuisGutierrezH <luisgh1401@gmail.com>
2025-02-10 15:59:27 -0600

    Creación de modulo custom

--- custom/items/scripts/custom_utils.py
+++ custom/items/scripts/custom_utils.py
@@ -4,10 +4,11 @@ import sys, simplejson
 from datetime import datetime, timedelta, date
 from copy import deepcopy
 
-from lkf_addons.addons.module_name.app import ModuleName
+from lkf_addons.addons.custom.app import Custom
 
 
-class ModuleName(ModuleName):
+class Custom(Custom):
 
     def __init__(self, settings, sys_argv=None, use_api=False):
         super().__init__(settings, sys_argv=sys_argv, use_api=use_api)
+    
