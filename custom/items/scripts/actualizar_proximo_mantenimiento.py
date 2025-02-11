4d6c2b49f3c2e5ff479cf671c666cde903eda67c
LuisGutierrezH <luisgh1401@gmail.com>
2025-02-11 14:13:09 -0600

    Actualizar proximo mantenimiento desde odt

---
+++ custom/items/scripts/actualizar_proximo_mantenimiento.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import sys, simplejson, copy
+from datetime import datetime, timedelta, date
+from dateutil.relativedelta import relativedelta
+from custom_utils import Custom
+
+from account_settings import *
+
+class Custom(Custom):
+    
+    def actualizar_proximo_mantenimiento(self, equipo, fecha):
+        serie = equipo[self.CATALOGO_EQUIPOS_OBJ_ID][self.f['serie_orden_trabajo']]
+        frecuencia = self.unlist(equipo[self.CATALOGO_EQUIPOS_OBJ_ID][self.f['frecuencia_mantenimniento']])
+        equipo = self.get_equipo(serie)
+        equipo_id = equipo['_id']
+        frecuencia_obj = frecuencia.split(' ')
+        frecuencia_obj = frecuencia_obj[0]
+        frecuencia_obj = int(frecuencia_obj)
+        fecha_n = datetime.strptime(fecha, '%Y-%m-%d')
+        fecha_n = fecha_n.date()
+        fecha_proximo_mantenimiento = fecha_n + relativedelta(months=frecuencia_obj)
+        answers = {
+            self.f['fecha_ultimo_mantenimiento']: fecha_n.strftime('%Y-%m-%d'),
+            self.f['fecha_proximo_mantenimiento']: fecha_proximo_mantenimiento.strftime('%Y-%m-%d'),
+        }
+        result = self.lkf_api.patch_multi_record(answers, self.INVENTARIO_DE_EQUIPOS, record_id=[equipo_id, ])
+    
+    
+        
+    def get_equipo(self, serie):
+        match_query ={ 
+         'form_id': self.INVENTARIO_DE_EQUIPOS,  
+         'deleted_at' : {'$exists':False},
+         f'answers.{self.f["serie_orden_trabajo"]}': serie
+         }
+        equipo = self.format_cr(self.cr.find(match_query), get_one=True)
+        return equipo
+ 
+
+if __name__ == '__main__':
+    #print("custom_scripts", 1)
+    module_obj = Custom(settings, sys_argv=sys.argv, use_api=True)
+    module_obj.console_run()
+
+    #print("custom_scripts", 2)
+    fecha = module_obj.answers[module_obj.f['fecha_orden_trabajo']]
+    equipos = module_obj.answers[module_obj.f['grupo_equipos_orden_trabajo']]
+    
+    for equipo in equipos:
+        module_obj.actualizar_proximo_mantenimiento(equipo, fecha)
--- custom/items/scripts/custom_utils.py
+++ custom/items/scripts/custom_utils.py
@@ -11,4 +11,20 @@ class Custom(Custom):
 
     def __init__(self, settings, sys_argv=None, use_api=False):
         super().__init__(settings, sys_argv=sys_argv, use_api=use_api)
-    
+        
+        self.ORDEN_DE_TRABAJO = 124767
+        self.INVENTARIO_DE_EQUIPOS = 124696
+        self.CATALOGO_EQUIPOS_OBJ_ID = "670f23d73ec5f4e5a8fd922e"
+        
+        self.f = {
+            'fecha_orden_trabajo':'67103ee34f6b049508fd9145',
+            'frecuencia_mantenimniento':'a00000000000000000000005',
+            'fecha_ultimo_mantenimiento':'a00000000000000000000011',
+            'fecha_proximo_mantenimiento':'a00000000000000000000012',
+            'instrumento_orden_trabajo':'6748b4da70017a0dc61603cc',
+            'serie_orden_trabajo':'a00000000000000000000004',
+            'grupo_equipos_orden_trabajo':'67103f80eb4737ed96b497bc',
+            'codigo_equipo':'a00000000000000000000001',
+            }
+
+        
