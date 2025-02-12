166c3a0a71c50a3f6cb1978c3f9ae1a4bf9887e8
LuisGutierrezH <luisgh1401@gmail.com>
2025-02-11 18:11:31 -0600

    Cuando no es odt reconoce el id para tomar la fecha

--- custom/items/scripts/actualizar_proximo_mantenimiento.py
+++ custom/items/scripts/actualizar_proximo_mantenimiento.py
@@ -18,7 +18,9 @@ class Custom(Custom):
         frecuencia_obj = int(frecuencia_obj)
         fecha_n = datetime.strptime(fecha, '%Y-%m-%d')
         fecha_n = fecha_n.date()
+        print('fecha_n', fecha_n)
         fecha_proximo_mantenimiento = fecha_n + relativedelta(months=frecuencia_obj)
+        print('***fecha_proximo_mantenimiento', fecha_proximo_mantenimiento)
         answers = {
             self.f['fecha_ultimo_mantenimiento']: fecha_n.strftime('%Y-%m-%d'),
             self.f['fecha_proximo_mantenimiento']: fecha_proximo_mantenimiento.strftime('%Y-%m-%d'),
@@ -35,6 +37,7 @@ class Custom(Custom):
          }
         equipo = self.format_cr(self.cr.find(match_query), get_one=True)
         return equipo
+    
  
 
 if __name__ == '__main__':
@@ -43,8 +46,15 @@ if __name__ == '__main__':
     module_obj.console_run()
 
     #print("custom_scripts", 2)
-    fecha = module_obj.answers[module_obj.f['fecha_orden_trabajo']]
-    equipos = module_obj.answers[module_obj.f['grupo_equipos_orden_trabajo']]
+    # fecha = module_obj.answers[module_obj.f['fecha_orden_trabajo']]
+    if module_obj.form_id == module_obj.ORDEN_DE_TRABAJO:
+        fecha = module_obj.answers[module_obj.f['fecha_orden_trabajo']]
+        equipos = module_obj.answers[module_obj.f['grupo_equipos_orden_trabajo']]
+    else:
+        #### de mi form_id obtenido ahora quiero sacarle el campo Fecha del servicio
+        #### y luego de eso quiero que me actualice el campo Fecha del servicio
+        fecha = module_obj.answers[module_obj.f['fecha_servicio']]
+        equipos = [module_obj.answers, ]
     
     for equipo in equipos:
         module_obj.actualizar_proximo_mantenimiento(equipo, fecha)
--- custom/items/scripts/custom_utils.py
+++ custom/items/scripts/custom_utils.py
@@ -8,13 +8,26 @@ from lkf_addons.addons.custom.app import Custom
 
 
 class Custom(Custom):
+        
+    def get_fecha_proximo_mantenimiento(self, form_id):
+        form_id = self.get_key_id(form_id) ##obtenemos el objeto de este    
+        print('++form_id2', form_id)
+        return form_id
+    
 
     def __init__(self, settings, sys_argv=None, use_api=False):
         super().__init__(settings, sys_argv=sys_argv, use_api=use_api)
         
+            
+            
+        
         self.ORDEN_DE_TRABAJO = 124767
+        print('PRUEBA_MICKEY',self.ORDEN_DE_TRABAJO)
         self.INVENTARIO_DE_EQUIPOS = 124696
         self.CATALOGO_EQUIPOS_OBJ_ID = "670f23d73ec5f4e5a8fd922e"
+        self.get_fecha_proximo_mantenimiento(self.form_id)
+        self.RECONOCE = self.form_id
+        print('PRUEBA_donalds',self.RECONOCE)
         
         self.f = {
             'fecha_orden_trabajo':'67103ee34f6b049508fd9145',
@@ -25,6 +38,7 @@ class Custom(Custom):
             'serie_orden_trabajo':'a00000000000000000000004',
             'grupo_equipos_orden_trabajo':'67103f80eb4737ed96b497bc',
             'codigo_equipo':'a00000000000000000000001',
+            'fecha_servicio':'670f1d404968f6557f57b20d',
             }
 
         
