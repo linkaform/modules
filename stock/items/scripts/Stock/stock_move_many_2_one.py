65d03df0c1b05ef5d69b6f1335070951ea37d45f
LuisGutierrezH <luisgh1401@gmail.com>
2025-02-26 11:58:13 -0600

    Error si no hay productos en el inventario

--- stock/items/scripts/Stock/stock_move_many_2_one.py
+++ stock/items/scripts/Stock/stock_move_many_2_one.py
@@ -9,12 +9,16 @@ from account_settings import *
 if __name__ == '__main__':
     stock_obj = Stock(settings, sys_argv=sys.argv, use_api=True)
     stock_obj.console_run()
-
-
-    response = stock_obj.move_one_many_one()
-    print('TODO: revisar si un create no estuvo bien y ponerlo en error o algo')
-    stock_obj.answers[stock_obj.f['inv_adjust_status']] =  'done'
     
+    try:
+        response = stock_obj.move_one_many_one()
+        stock_obj.answers[stock_obj.f['inv_adjust_status']] =  'done'
+    
+    except Exception as e:
+        stock_obj.answers[stock_obj.f['inv_adjust_status']] =  'error'
+        stock_obj.answers[stock_obj.f['stock_move_comments']] =  e
+        print(e)
+        print('TODO: revisar si un create no estuvo bien y ponerlo en error o algo')
 
     sys.stdout.write(simplejson.dumps({
         'status': 101,
