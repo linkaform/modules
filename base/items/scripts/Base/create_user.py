a8a2c0339f02dc1c752b2f84dea6fd0cf169b994
paco soto <francisotomtz@gmail.com>
2025-03-06 16:47:35 -0600

    se mueve script de creacion de usuarios a base

--- base/items/scripts/Base/create_user.py
+++ base/items/scripts/Base/create_user.py
@@ -7,20 +7,19 @@ import sys, simplejson, json
 from linkaform_api import settings
 from account_settings import *
 
-from accesos_utils import Accesos
+from base_utils import Base
 
-class Accesos(Accesos):
+class Base(Base):
 
     def __init__(self, settings, sys_argv=None, use_api=False):
         super().__init__(settings, sys_argv=sys_argv, use_api=use_api)
-        self.load(module='Base', **self.kwargs)
 
 if __name__ == "__main__":
-    acceso_obj = Accesos(settings, sys_argv=sys.argv)
-    acceso_obj.console_run()
-    user_data = acceso_obj.answers
+    base_obj = Base(settings, sys_argv=sys.argv)
+    base_obj.console_run()
+    user_data = base_obj.answers
 
-    response = acceso_obj.Base.create_user_account(user_data=user_data)
+    response = base_obj.create_user_account(user_data=user_data)
 
     sys.stdout.write(simplejson.dumps({
         'response': response
