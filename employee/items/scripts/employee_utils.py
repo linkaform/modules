a147d09e0ac1b554cd481674a9e85c50ce83ab46
paco soto <francisotomtz@gmail.com>
2025-03-06 16:48:55 -0600

    pequenio cambio en base utils y se crea employee utils

--- employee/items/scripts/employee_utils.py
+++ employee/items/scripts/employee_utils.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 from linkaform_api import base
-from lkf_addons.addons.base.app import Base
+from lkf_addons.addons.employee.app import Employee
 
 
-class Base(Base):
-    print('Entra a base utils')
+class Employee(Employee):
+    print('Entra a employee utils')
 
     def __init__(self, settings, sys_argv=None, use_api=False):
         super().__init__(settings, sys_argv=sys_argv, use_api=use_api)
