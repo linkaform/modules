4ceae297bc5a886116d697b495b54e9b7307e418
Linkaform Develop Team <backend@linkaform.com>
2025-05-17 22:11:53 -0600

    mejra en funcion de calcluta_tiempo_traslados

--- accesos/items/scripts/Accesos/bitacora_rondines.py
+++ accesos/items/scripts/Accesos/bitacora_rondines.py
@@ -9,29 +9,32 @@ from accesos_utils import Accesos
 class Accesos(Accesos):
 
     def calcluta_tiempo_traslados(self):
-        print('calculadndo tiemps')
-        print('answesrs', simplejson.dumps(self.answers, indent=3))
         fecha_inicio = self.answers[self.f['fecha_inicio_rondin']]
-        print('fecha_inicio=',fecha_inicio)
         fecha_inicio = self.date_2_epoch(self.answers[self.f['fecha_inicio_rondin']])
-        print('epoch fecha_inicio=',fecha_inicio)
         areas_visitadas = self.answers[self.f['grupo_areas_visitadas']]
         fecha_inspeccion = 0
         duracion_total = 0
-        for area in areas_visitadas:
-            print('area', area)
-            fecha_inspeccion = area.get(self.f['fecha_inspeccion_area'])
-            print('fecha_inspeccion', fecha_inspeccion)
+        last_inspection = fecha_inicio
+        set_dates = {}
+        for idx, area in enumerate(areas_visitadas):
             fecha_inspeccion = self.date_2_epoch(fecha_inspeccion)
-            print('epoch fecha_inspeccion', fecha_inspeccion)
-            print('area', area)
-            duracion = fecha_inspeccion - fecha_inicio
-            print('duracion', duracion)
-            area[self.f['duracion_traslado_area']] = round(duracion / 60,2)
+            set_dates[idx] = fecha_inspeccion
+        print('set dates', set_dates)
+        areas_dates = set_dates.values()
+        areas_dates.sort()
+        print('area dates', areas_dates)
+        for secs in areas_dates:
+            print('secs', secs)
+            duracion = 0
+            fecha_inspeccion = area.get(self.f['fecha_inspeccion_area'])
+            if fecha_inspeccion:
+                if fecha_inspeccion > last_inspection:
+                    last_inspection = fecha_inspeccion
+                duracion = (fecha_inspeccion - fecha_inicio)/60
+            area[self.f['duracion_traslado_area']] = round(duracion)
             duracion_total += duracion
-        if fecha_inspeccion:
-            print('duracion_total', duracion_total)
-            self.answers[self.f['duracion_rondin']] = round(duracion_total / 60,2)
+        if last_inspection > fecha_inicio:
+            self.answers[self.f['duracion_rondin']] = round((last_inspection - fecha_inicio)/60,2)
         return True
 
 
@@ -40,9 +43,9 @@ if __name__ == "__main__":
     acceso_obj.console_run()
     #-FILTROS
     acceso_obj.calcluta_tiempo_traslados()
-    print('answers', acceso_obj.answers)
-    sys.stdout.write(simplejson.dumps({
-        'status': 101,
-        'replace_ans': acceso_obj.answers
-    }))
+    # print('answers', acceso_obj.answers)
+    # sys.stdout.write(simplejson.dumps({
+    #     'status': 101,
+    #     'replace_ans': acceso_obj.answers
+    # }))
 
