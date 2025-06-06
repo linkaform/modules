f7c16efca9b0a25db9d4dfc6e6c28c2049d809a9
Linkaform Develop Team <backend@linkaform.com>
2025-06-05 22:07:05 -0600

    se crea script para que busque todas las formas de dicha carpeta y cree sus repuesta por imagenes y comentarios

--- inspeccion_hoteleria/items/scripts/InspeccionHoteleria/save_comments_media.py
+++ inspeccion_hoteleria/items/scripts/InspeccionHoteleria/save_comments_media.py
@@ -6,48 +6,9 @@ from inspeccion_hoteleria_utils import Inspeccion_Hoteleria
 from account_settings import *
 
 
-preguntas_revisar = ['67ed67dfc61cd5bd89f317f9',
-                    '67ed67dfc61cd5bd89f317fa',
-                    '67ed662ab178b35ea5a103b8',
-                    '67ed662ab178b35ea5a103b9']
 
 class Inspeccion_Hoteleria(Inspeccion_Hoteleria):
 
-    def actualiza_status_habitacion(self):
-        """Acutaliza status de habitacion seguns su nombre.
-        Obtiene del registro el nombre de la habitacion y lo envia para su actualizacion
-
-        Returns:
-            True, si se actualizo correctamente
-            False, si fallo la actualizacion
-        """
-        self.load(module='Location', **self.kwargs)
-        name = self.answers[self.Location.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID][self.Location.f['area']]
-        print('nombre de habitacion', name)
-        res = self.Location.update_status_habitacion(name, 'inactiva')
-        print('nombre de res', res)
-        if res.get('status_code') == 202:
-            return True
-        else:
-            return False
-
-    def check_pending_answers(self):
-        status = 'proceso'
-        t = 0
-        for q_id in preguntas_revisar:
-            if self.answers.get(q_id):
-                if self.answers.get(q_id) in ('si','no', 'sí'):
-                   t += 1
-        if t == len(preguntas_revisar):
-            status = 'completada'
-        return status
-
-    def update_status_record(self,  status, msg_comentarios='' ):
-        print('status...', status)
-        answers = {self.field_id_status:status}
-        res = self.lkf_api.patch_multi_record( answers = answers, form_id=self.form_id, record_id=[self.record_id])
-        print('res',res)
-        return res
 
     def set_comentarios(self):
         comentarios = ''
@@ -142,27 +103,29 @@ class Inspeccion_Hoteleria(Inspeccion_Hoteleria):
             'comments': self.comments,
             'media': self.media,
             'field_label': self.field_label,
+            'created_at': self.created_at,
             })
         return self.create(data, collection='inspeccion_hoteleria')
 
+    def get_record(self, folder_id):
+        forms = self.lkf_api.get_folder_forms(folder_id)
+        forms = [x['form_id'] for x in forms]
+        records = self.cr.find({'form_id': {'$in': forms}},{'answers': 1, 'form_id': 1, 'folio': 1, 'created_at': 1})        
+        return records
+        
         
 if __name__ == '__main__':
     module_obj = Inspeccion_Hoteleria(settings, sys_argv=sys.argv, use_api=False)
     module_obj.console_run()
-    print('folio', module_obj.folio)
-    score_per_page = module_obj.get_grading_obj()
-    res = module_obj.save_results(score_per_page)
-    starting_status = module_obj.answers[module_obj.field_id_status]
-    status = module_obj.check_pending_answers() 
-    # module_obj.actualiza_status_habitacion()
-    # Todo ver q hacer si no se actualzia bien el status de la habitacion....
-    print('Record starting_status', starting_status)
-    print('status to update', status)
-    if starting_status != status:
-        res = module_obj.update_status_record(status)
-        print('r1111es',res['status_code'])
-        if res.get('status_code') in (200, 201):
-            print('updating')
-            update_res = module_obj.cr.update_one({'folio':module_obj.folio}, {'$set':{'editable': False}})
-            print('updating',module_obj.cr)
+    records = module_obj.get_record(131836)
+    for record in records:
+        print('record', record['folio'])
+        module_obj.record_id = record.get('_id')
+        module_obj.answers = record.get('answers')
+        module_obj.form_id = record.get('form_id')
+        module_obj.folio = record.get('folio')
+        module_obj.created_at = record.get('created_at')
+        score_per_page = module_obj.get_grading_obj()
+        res = module_obj.save_results(score_per_page)
+        print('saved on', res)
 
