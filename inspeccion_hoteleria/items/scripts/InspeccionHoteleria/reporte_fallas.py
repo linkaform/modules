72afd226e7f9335a6d66eee1cbf5537ea3d82778
paco soto <francisotomtz@gmail.com>
2025-06-25 10:16:25 -0600

    refactor: Se cambia fallas_hotel para obtenerse de forma dinamica y no con un diccionario estatico y se ajustan fallas para obtenerse con el nuevo fallas_dict

--- inspeccion_hoteleria/items/scripts/InspeccionHoteleria/reporte_fallas.py
+++ inspeccion_hoteleria/items/scripts/InspeccionHoteleria/reporte_fallas.py
@@ -20,6 +20,31 @@ class Inspeccion_Hoteleria(Inspeccion_Hoteleria):
         self.load(module='Location', **self.kwargs)
 
         self.cr_inspeccion = self.net.get_collections(collection='inspeccion_hoteleria')
+        
+        self.labels_to_exclude = [
+            'Nombre de la Camarista',
+            'Habitación Remodelada',
+            'Fotografía',
+            'Comentarios',
+            '¿La habitación es suite?',
+            'Comentarios Generales'
+        ]
+        
+        self.ids_to_exclude = [
+            '67f81a13a48f0a56cf242558',
+            '680bb736c2009c72987ba955',
+            '680bb736c2009c72987ba957',
+            '680bb736c2009c72987ba959',
+            '680bb736c2009c72987ba95b',
+            '680bb736c2009c72987ba95d',
+            '680bb736c2009c72987ba95f',
+            '680bb736c2009c72987ba961',
+            '680bb736c2009c72987ba963',
+            '680bb736c2009c72987ba965',
+            '680bb736c2009c72987ba967'
+        ]
+        
+        self.fallas_dict = {}
 
         self.f.update({
             'tipo_ubicacion': '683d2970bb02c9bb7a0bfe1c',
@@ -60,225 +85,6 @@ class Inspeccion_Hoteleria(Inspeccion_Hoteleria):
             'WYNDHAM GARDEN MCALLEN': 'MCALLEN',
         }
 
-        self.fallas_hotel = {
-            'foco_de_pasillo_afuera_de_la_habitacion': '67f0844734855c523e1390d8',
-            'plafon_fuera_de_la_habitacion': '67f0844734855c523e1390db',
-            'esquinero': '67f0844734855c523e1390de',
-            'placa_de_numero_de_habitacion': '67f0844734855c523e1390e1',
-            'placa_de_no_fumar': '67f0844734855c523e1390e4',
-            'timbre': '67f7e39ceffd2a15e842ba54',
-            'puerta_de_entrada_exterior': '67f0844734855c523e1390ea',
-            'chapa_electronica_exterior': '67f0844734855c523e1390ed',
-            'contra_chapa': '67f0844734855c523e1390f0',
-            'puerta_de_entrada_interior': '67f0844734855c523e1390f3',
-            'bisagras_de_puerta_de_entrada': '67f0844734855c523e1390f6',
-            'marco_de_puerta_de_entrada': '67f0844734855c523e1390f9',
-            'sardinel': '67f0844734855c523e1390fc',
-            'auto_cierre_de_puerta_de_entrada': '67f0844734855c523e1390ff',
-            'buen_olor': '67f0844734855c523e139102',
-            'mirilla': '67f0844734855c523e139105',
-            'plano_evacuacion': '67f0844734855c523e139108',
-            'hueso_de_pollo__/_gomita': '67f0844734855c523e13910b',
-            'chapa_interior_-_pasador': '67f0844734855c523e13910e',
-            'tope_de_puerta_de_entrada': '67f0844734855c523e139111',
-            'colgante_no_molestar': '67f0844734855c523e139114',
-            'colgante_greener_stay': '67f0844734855c523e139117',
-            'guardapolvo': '67f0844734855c523e13911a',
-            'apagador_de_entrada_/_switch_maestro': '67f0844734855c523e13911d',
-            'ahorrador_de_energia': '680bb9ae683baa875e7baa1f',
-            'luminaria_de_entrada': '67f0844734855c523e139120',
-            'plafon_entrada': '67f0844734855c523e139123',
-            'piso_laminado / Alfombra entrada': '67f0844734855c523e139126',
-            'zoclo_de_entrada': '67f0844734855c523e139129',
-            'tapiz_/_pintura_de_entrada': '67f0844734855c523e13912c',
-            'doble_puerta_(Chapa,_marco,_cinta tesamol)': '67f7e39ceffd2a15e842ba57',
-            'puerta_de_comunicacion': '67ed63bafba6620598f318a2',
-            'chapa_/_seguro': '67ed63f3f7b2bd2aa34d9e6a',
-            'contra_chapa': '67ed63f3f7b2bd2aa34d9e6b',
-            'tapa_ciega': '67ed63f3f7b2bd2aa34d9e6c',
-            'marco_de_puerta_de_comunicacion': '67ed63f3f7b2bd2aa34d9e6d',
-            'cinta_tesamol': '67ed63f3f7b2bd2aa34d9e6e',
-            'luminaria_de_closet': '67ed64ce17c7a71475f318d2',
-            'puertas_de_closet': '674a006c98371866c7160370',
-            'manijas_/perillas_de_puerta_de_closet': '67ed64ce17c7a71475f318d3',
-            'marco_de_puertas_de_closet': '67ed64ce17c7a71475f318d4',
-            'broches_perico': '67ed64ce17c7a71475f318d5',
-            'plafon_de_closet': '67ed64ce17c7a71475f318d6',
-            'rack_tubular': '67ed64ce17c7a71475f318d7',
-            'ganchos_(lisos_y_con_pinza)': '674a001bcc56e7f30c939286',
-            'bolsa_de_lavanderia': '674a006c98371866c716036a',
-            'lista_de_lavanderia': '674a006c98371866c716036b',
-            'rack_para_plancha': '674a006c98371866c716036c',
-            'plancha': '674a006c98371866c716036f',
-            'tabla_de_planchar': '674a006c98371866c716036e',
-            'piso_laminado_/_alfombra_de_closet': '674a006c98371866c716036d',
-            'zoclo_de_closet': '674a006c98371866c7160371',
-            'tapiz_/_pintura_de_closet': '67ed64ce17c7a71475f318d8',
-            'caja_de_seguridad': '67ed64ce17c7a71475f318d9',
-            'instrucciones_de_caja_de_seguridad': '67ed64ce17c7a71475f318da',
-            'porta_equipaje_(tijera_o_integrado_en_mueble_de_TV)': '67ed64ce17c7a71475f318db',
-            'detector_de_humo': '67ed650d8ec7fb6f794d9ee7',
-            'bocina': '67ed650d8ec7fb6f794d9ee8',
-            'sprinkle': '67ed650d8ec7fb6f794d9ee9',
-            'luz_estroboscopica_(solo_en_habitacion_de_discapacitados)': '67ed650d8ec7fb6f794d9eea',
-            'habitacion_para_discapacitados_(mirilla,_silla_portatil_en_baño,_lavabo_adaptado,_telefono_especial,_push_en_cajones,_push_de_lampara,_aro_en_bastones_de_cortina)': '67ed650d8ec7fb6f794d9eeb',
-            'puerta_de_baño': '6749fe84b841cb116d0755eb',
-            'apagador_de_baño': '67f035888532823bf4138dc0',
-            'bisagras_de_puerta_de_baño': '6749ffcb33e84f4eb44a19bd',
-            'colgante_de_programa_de_reuso_de_toallas_(en_chapa,_tubo_curvo_o_gancho)': '6749ffcb33e84f4eb44a19be',
-            'tope_de_puerta_de_baño': '6749ffcb33e84f4eb44a19bf',
-            'marco_de_puerta_de_baño': '6749ffcb33e84f4eb44a19c0',
-            'sardinel': '6749ffcb33e84f4eb44a19c1',
-            'chapa_exterior_/_interior': '6749ffcb33e84f4eb44a19c2',
-            'contra_chapa_de_puerta_de_baño': '6749ffcb33e84f4eb44a19c3',
-            'espejo_de_cuerpo_entero': '6749ffcb33e84f4eb44a19c4',
-            'gancho': '6749ffcb33e84f4eb44a19c5',
-            'plafon_de_baño': '6749ffcb33e84f4eb44a19c6',
-            'muros_de_baño': '6749ffcb33e84f4eb44a19c7',
-            'wc(_sanitario,_tapa,_herrajes)': '6749ffcb33e84f4eb44a19c8',
-            'valvulas_de_paso_(sin_fuga)': '6749ffcb33e84f4eb44a19c9',
-            'cesto_de_basura_de _baño': '6749ffcb33e84f4eb44a19ca',
-            'papel_higienico': '6749ffcb33e84f4eb44a19cb',
-            'portarrollos': '6749ffcb33e84f4eb44a19cc',
-            'piso_de_baño': '6749ffcb33e84f4eb44a19cd',
-            'coladera_de_piso_de_baño': '6749ffcb33e84f4eb44a19ce',
-            'zoclo_de_baño': '6749ffcb33e84f4eb44a19cf',
-            'rejilla_de_extraccion': '6749ffcb33e84f4eb44a19d0',
-            'rack_toallero_o_marimba': '6749ffcb33e84f4eb44a19d1',
-            'toallas_de_Baño': '67ed6592b4c33b95759a4f77',
-            'luminaria_de_tina': '67ed662ab178b35ea5a103b0',
-            'tubo_curvo': '67ed662ab178b35ea5a103b1',
-            'cortina_de_baño': '67ed662ab178b35ea5a103b2',
-            'tapete_de_felpa': '67ed662ab178b35ea5a103b3',
-            'sin_tendedero_retractil': '67ed662ab178b35ea5a103b4',
-            'paredes_de_tina': '67ed662ab178b35ea5a103b5',
-            'cebolleta': '67ed662ab178b35ea5a103b6',
-            'monomando_/_llaves_de_regadera': '67ed662ab178b35ea5a103b7',
-            'agua_fria_/_caliente': '67ed662ab178b35ea5a103b8',
-            'desague': '67ed662ab178b35ea5a103b9',
-            'barras_de_seguridad': '67ed662ab178b35ea5a103ba',
-            'jabonera': '680bbf54e262646da62818f7',
-            'jabon_de_cuerpo': '680bbf54e262646da62818fa',
-            'rack_dispensador': '67ed662ab178b35ea5a103bd',
-            'botellas_de_shampoo_-_acondicionador_-_gel_de_baño': '67ed662ab178b35ea5a103be',
-            'nariz_de_tina': '67ed662ab178b35ea5a103bf',
-            'automatico_de_tina_/_push': '67ed662ab178b35ea5a103c0',
-            'tina_base_(antiderrapante)_y_faldon': '67ed662ab178b35ea5a103c1',
-            'tapon_de_tina': '67ed662ab178b35ea5a103c2',
-            'cristal_de_regadera,_soporte_de_regadera,_puerta': '680bbf54e262646da62818fd',
-            'chapeton': '67ed662ab178b35ea5a103c3',
-            'emboquillado_(silicon)': '67ed662ab178b35ea5a103c4',
-            'luminarias_de_lavabo': '67ed67dfc61cd5bd89f317f2',
-            'espejo_de_Lavabo': '67ed67dfc61cd5bd89f317f3',
-            'sin_brackets': '680bc1bce1e8a2ba776c38fa',
-            'espejo_de_vanidad': '67ed67dfc61cd5bd89f317f4',
-            'lavabo_superficie_y_faldon': '67ed67dfc61cd5bd89f317f5',
-            'ovalin': '67ed67dfc61cd5bd89f317f6',
-            'monomando_/_llave_mezcladora': '67ed67dfc61cd5bd89f317f7',
-            'automatico_de_lavabo': '67ed67dfc61cd5bd89f317f8',
-            'agua_fria_/_Caliente': '67ed67dfc61cd5bd89f317f9',
-            'desague': '67ed67dfc61cd5bd89f317fa',
-            'olvido_algo_-_tent_card': '680bc1bce1e8a2ba776c38fd',
-            'kleenera': '67ed67dfc61cd5bd89f317fc',
-            'kleenex_-panuelos': '67ed67dfc61cd5bd89f317fd',
-            'contacto_falla_tierra': '67f0398ed7a42953a0cbaef3',
-            'secadora_de_pelo': '67ed67dfc61cd5bd89f317fe',
-            'bolsa_de_secadora': '67f0398ed7a42953a0cbaef4',
-            'amenidades_(solo_vIPS)': '680bc1bce1e8a2ba776c3900',
-            'rack-_dispensador': '67ed67dfc61cd5bd89f31800',
-            'botellas_de_jabon_de_manos_y_crema corporal': '67ed67dfc61cd5bd89f31801',
-            'jabon_facial_y_crema_corporal': '680bc1bce1e8a2ba776c3903',
-            'toallas_(_manos,_facial)': '67ed67dfc61cd5bd89f31803',
-            'aro_o_rack_toallero': '67ed67dfc61cd5bd89f31804',
-            'valvulas_(sin_fuga)': '67ed67dfc61cd5bd89f31805',
-            'trampa_de_lavabo_/_tubo_cespol': '67ed67dfc61cd5bd89f31806',
-            'rejilla_de_inyeccion': '674a00cf5f05ce94969392ba',
-            'rejilla_de_retorno': '674a0257ce0d9173204a19c5',
-            'apagadores': '674a0257ce0d9173204a19c6',
-            'piso_/_alfombra': '674a0257ce0d9173204a19c7',
-            'tapiz_/_pintura_de_muros': '674a0257ce0d9173204a19c8',
-            'zoclos': '674a0257ce0d9173204a19c9',
-            'a_/_c_(sin_ruido)': '674a0257ce0d9173204a19ca',
-            'termostato': '674a0257ce0d9173204a19cb',
-            'lampara_de_pie': '674a0257ce0d9173204a19cc',
-            'cabecera': '674a0257ce0d9173204a19cd',
-            'almohadas_(funda_y_protector)': '674a0257ce0d9173204a19ce',
-            'ropa_de_cama_(sabanas,_duvet,_rodapie,_etc)': '674a0257ce0d9173204a19cf',
-            'tendido_de_cama': '674a0257ce0d9173204a19d0',
-            'perchero': '680bca79343dc15af751bbc9',
-            'buros_(cajones)': '674a0257ce0d9173204a19d1',
-            'multi-_contacto': '674a0257ce0d9173204a19d2',
-            'lamparas_buro_o_lampara_de_cabecera': '674a0257ce0d9173204a19d3',
-            'luz_de_lectura': '67ffebc902d1b72e5551bbf9',
-            'reloj_despertador_/_Cubitime': '674a0257ce0d9173204a19d4',
-            'telefono_/_caratula': '674a0257ce0d9173204a19d5',
-            'tent_card_reuso_de_sabanas': '674a0257ce0d9173204a19d6',
-            'cuadros_decorativos': '674a0257ce0d9173204a19d7',
-            'sofa_doble_/_sillon_individual_(hab_sencilla)': '680bca79343dc15af751bbcc',
-            'sillas_(hab_sencilla)': '67f03e568d1b646b9e365acc',
-            'sillon_(hab_doble)': '674a0257ce0d9173204a19d9',
-            'mesa_de_apoyo': '674a0257ce0d9173204a19da',
-            'bandeja_para_cafetera': '674a0257ce0d9173204a19db',
-            'cafetera': '674a0257ce0d9173204a19dc',
-            'cafe _(regular_y_descafeinado)': '674a0257ce0d9173204a19dd',
-            'te': '674a0257ce0d9173204a19de',
-            'kit_de_complementos': '674a0257ce0d9173204a19df',
-            'tazas_de_ceramica_con_blonda': '67ed67dfc61cd5bd89f31809',
-            'vasos_vidrio_con_blonda': '67ed67dfc61cd5bd89f3180a',
-            'botellas_con_agua': '67ed67dfc61cd5bd89f3180b',
-            'vaso_termico_para_cafe_en_bolsa': '680bca79343dc15af751bbcf',
-            'vaso_desechable_con_tapa_en_bolsa': '680bca79343dc15af751bbd2',
-            'hielera_con_bolsa_y_tapa': '67ed67dfc61cd5bd89f3180e',
-            'cortinas_de_frescura': '67ed67dfc61cd5bd89f31811',
-            'cortinas_black out': '67ed67dfc61cd5bd89f31812',
-            'cortineros_-_riel': '67ed67dfc61cd5bd89f31813',
-            'bastones_de_cortinas': '67ed67dfc61cd5bd89f31814',
-            'ventanal_cerrado_/_pasamanos': '67f08a48e97471d4cef84404',
-            'balcon': '680bca79343dc15af751bbd5',
-            'perno_de_seguridad_balcon': '680bca79343dc15af751bbd8',
-            'rieles_de_ventanal_de_balcon': '680bca79343dc15af751bbdb',
-            'barandal_de_balcon': '680bca79343dc15af751bbde',
-            'sardinel_de_balcon': '680bca79343dc15af751bbe1',
-            'sillas_de_balcon': '680bca79343dc15af751bbe4',
-            'macetas_decorativas': '680bca79343dc15af751bbe7',
-            'internet': '67ffed2115a48b68c37ba998',
-            'sin_insectos': '67ffed2115a48b68c37ba99b',
-            'lampara_de_escritorio': '674a0312d080d8ec99075621',
-            'multi-_contactos': '674a033bce0d9173204a1a16',
-            'escritorio': '674a033bce0d9173204a1a17',
-            'silla_de_escritorio_/_ergonomica': '674a033bce0d9173204a1a18',
-            'reglamento_interno': '67ed67dfc61cd5bd89f3181a',
-            'tips_de_seguridad': '67ed67dfc61cd5bd89f3181b',
-            'cesto_de_basura': '67ed67dfc61cd5bd89f3181c',
-            'mueble': '674a03706d15fa34f3828411',
-            'television_(imagen,_volumen)': '674a03c186f66205504a1a55',
-            'guia_de_canales': '674a03c186f66205504a1a56',
-            'control_remoto': '674a03c186f66205504a1a57',
-            'directorio_de_servicios_qr': '674a03c186f66205504a1a5a',
-            'cajones': '674a03c186f66205504a1a58',
-            'menu_de_room_service': '674a03c186f66205504a1a59',
-            'rejilla_de_inyeccion': '67f04106303b619b9cf842c5',
-            'rejilla_de_retorno': '67f04218dd311d4ea0138e2c',
-            'apagadores': '67f04218dd311d4ea0138e2d',
-            'piso_/_alfombra': '67f04218dd311d4ea0138e2e',
-            'tapiz_/_pintura de muros': '67f04218dd311d4ea0138e2f',
-            'cuadros_decorativos': '67ffef2aca29d5ca7b7ba9c7',
-            'zoclos': '67f04218dd311d4ea0138e30',
-            'a_/_C_(sin_ruido)': '67f04218dd311d4ea0138e31',
-            'termostato_de_sala': '67f04218dd311d4ea0138e32',
-            'lampara_de_pie_de_sala': '67f04218dd311d4ea0138e33',
-            'mueble_-_sofa_de_sala': '67f04218dd311d4ea0138e34',
-            'tV sala suite': '680bca79343dc15af751bbec',
-            'mesa_de_centro': '67f04218dd311d4ea0138e35',
-            'mesa_de_comedor': '67ffef2aca29d5ca7b7ba9c8',
-            'sillas_de_comedor': '67ffef2aca29d5ca7b7ba9cb',
-            'lampara_de_techo_/_socket': '67ffef2aca29d5ca7b7ba9ce',
-            'microondas': '67f04218dd311d4ea0138e36',
-            'frigobar': '67f04218dd311d4ea0138e37',
-            'cocineta': '67f04218dd311d4ea0138e38',
-            'tarja': '680bca79343dc15af751bbef',
-        }
-
     def normalize_types(self, obj):
         if isinstance(obj, list):
             return [self.normalize_types(x) for x in obj]
@@ -298,6 +104,9 @@ class Inspeccion_Hoteleria(Inspeccion_Hoteleria):
         texto = texto.replace(" ", "_")
         texto = unicodedata.normalize('NFKD', texto).encode('ascii', 'ignore').decode('ascii')
         return texto
+    
+    def limpia_keys_dict(self, dic):
+        return {k.replace('.', '_'): v for k, v in dic.items()}
 
     def get_hoteles(self):
         query = [
@@ -798,7 +607,8 @@ class Inspeccion_Hoteleria(Inspeccion_Hoteleria):
     
         group_fields = {"_id": "$form_id"}
         total_fields = {"_id": None}
-        for nombre_falla, id_falla in self.fallas_hotel.items():
+        fallas_dict_without_dots = self.limpia_keys_dict(self.fallas_dict)
+        for nombre_falla, id_falla in fallas_dict_without_dots.items():
             group_fields[nombre_falla] = { # type: ignore
                 "$sum": {
                     "$cond": [
@@ -1430,6 +1240,8 @@ class Inspeccion_Hoteleria(Inspeccion_Hoteleria):
 
         forms_id_list = self.get_forms_id_list(hoteles)
         print("forms_id_list", forms_id_list)
+        
+        self.get_labels(forms_id_list=forms_id_list)
 
         total_habitaciones = self.get_cantidad_habitaciones(ubicaciones_list=hoteles)
 
@@ -1473,6 +1285,19 @@ class Inspeccion_Hoteleria(Inspeccion_Hoteleria):
 
     def get_pdf(self, record_id, template_id=131835, name_pdf='Inspeccion de Habitacion'):
         return self.lkf_api.get_pdf_record(record_id, template_id=template_id, name_pdf=name_pdf, send_url=True)
+    
+    def get_labels(self, forms_id_list=[]):
+        if len(forms_id_list) > 1:
+            forms_id_list = [self.REVISON_HABITACION] # * Toma el id de la forma maestro para no demorar tanto
+        for form_id in forms_id_list:
+            form_data = self.lkf_api.get_form_id_fields(form_id)
+            fields = form_data[0].get('fields', [])
+            for field in fields:
+                if not field.get('catalog'):
+                    if not field.get('label') in self.labels_to_exclude and not field.get('field_id') in self.ids_to_exclude:
+                        self.fallas_dict.update({
+                            self.normaliza_texto(field.get('label')): field.get('field_id')
+                        })
 
 if __name__ == '__main__':
     module_obj = Inspeccion_Hoteleria(settings, sys_argv=sys.argv, use_api=True)
