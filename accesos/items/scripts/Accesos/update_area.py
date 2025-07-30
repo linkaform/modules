bc53fe68d42abaf51639ea67bf865d3acd8edc7f
paco soto <francisotomtz@gmail.com>
2025-07-30 11:29:56 -0600

    feat: Se agrega la creacion de area desde forma a partir del script

--- accesos/items/scripts/Accesos/update_area.py
+++ accesos/items/scripts/Accesos/update_area.py
@@ -11,13 +11,16 @@ class Accesos(Accesos):
 
     def __init__(self, settings, folio_solicitud=None, sys_argv=None, use_api=False, **kwargs):
         super().__init__(settings, sys_argv=sys_argv, use_api=use_api, **kwargs)
+        self.load(module='Location', **self.kwargs)
 
         self.configuracion_area = {
             'qr_area': '68487646684fe30a8f9f3ef3',
             'foto_area': '68487646684fe30a8f9f3ef4',
             'ubicacion': '663e5c57f5b8a7ce8211ed0b',
             'area': '663e5d44f5b8a7ce8211ed0f',
-            'option': '68487646684fe30a8f9f3ef2'
+            'option': '68487646684fe30a8f9f3ef2',
+            'create_area': '688a33d9e61fcd2c299ff39e',
+            'nombre_nueva_area': '688a33d9e61fcd2c299ff39f',
         }
 
         self.area_update = {
@@ -56,6 +59,8 @@ class Accesos(Accesos):
             'option': data.get(self.configuracion_area['option'], ''),
             'ubicacion': data.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.configuracion_area['ubicacion'], ''),
             'area': data.get(self.AREAS_DE_LAS_UBICACIONES_CAT_OBJ_ID, {}).get(self.configuracion_area['area'], ''),
+            'create_area': True if data.get(self.configuracion_area['create_area']) == 'crear_nueva_area' else False,
+            'nombre_nueva_area': data.get(self.configuracion_area['nombre_nueva_area'], '')
         })
 
         return formatted_data
@@ -191,12 +196,118 @@ class Accesos(Accesos):
         else:
             return {"status": "error", "message": "Unexpected error occurred."}
 
+    def get_contact_details(self, direccion):
+        selector = {}
+        
+        selector.update({
+            f"answers.{self.area_update['nombre_direccion']}": direccion, # type: ignore
+        })
+            
+        fields = [
+            "_id",
+            f"answers.{self.area_update['nombre_direccion']}", # type: ignore
+            f"answers.{self.area_update['pais_area']}",
+            f"answers.{self.area_update['direccion_area']}",
+            f"answers.{self.area_update['colonia_area']}",
+            f"answers.{self.area_update['geolocalizacion_area']}",
+            f"answers.{self.area_update['ciudad_area']}",
+        ]
+        
+        mango_query = {
+            "selector": selector,
+            "fields": fields,
+            "limit": 1,
+        }
+        res = self.lkf_api.search_catalog(131890, mango_query)
+        res = self.unlist(res)
+        res.pop('_id', None)
+        res.pop('_rev', None)
+        res.pop('created_at', None)
+        res.pop('updated_at', None)
+        return res if res else {}
+
+    def create_new_area(self, data):
+        if not data.get('ubicacion'):
+            msg = 'Debes dejar una ubicacion seleccionada en el catalogo.'
+            self.LKFException({'msg': msg, 'title': 'Creacion de area'})
+        if not data.get('nombre_nueva_area'):
+            msg = 'Debes de rellenar el campo nombre del area para crear un area valida.'
+            self.LKFException({'msg': msg, 'title': 'Creacion de area'})
+            
+        contact_details = self.get_contact_details(data.get('ubicacion', {}))
+        
+        answers = {
+            self.mf['nombre_area']: data.get('nombre_nueva_area'),
+            self.Location.UBICACIONES_CAT_OBJ_ID: {
+                self.mf['nombre_ubicacion_salida']: data.get('ubicacion', ''),
+            },
+            self.Location.TIPO_AREA_OBJ_ID: {
+                self.area_update['tipo_area']: 'Área Pública'
+            },
+            self.CONTACTO_CAT_OBJ_ID: contact_details,
+            self.area_update['estatus']: 'activa',
+            self.area_update['estatus_area']: 'disponible',
+        }
+        
+        response = self.create_register(
+            module='Accesos',
+            process='Creacion de una area',
+            action='rondines',
+            file='accesos/app.py',
+            form_id=131892, #TODO Modularizar este ID
+            answers=answers
+        )
+        
+        if response.get('status') != 'success':
+            msg = 'El area no fue creada correctamente, solicita a soporte revisar logs.'
+            self.LKFException({'msg': msg, 'title': 'Creacion de area'})
+
+        return response
+    
+    def create_register(self, module: str, process: str, action: str, file: str, form_id: int, answers: dict):
+        """Crea un registro en Linkaform con los metadatos y respuestas proporcionadas.
+
+        Args:
+            module (str): El nombre del módulo que está ejecutando la acción.
+            process (str): El nombre del proceso que se está ejecutando.
+            action (str): El nombre del script que se está ejecutando.
+            file (str): La ruta del archivo donde se encuentra el app del modulo utilizado(Ej. jit/app.py).
+            form_id (str): El ID de la forma en Linkaform.
+            answers (dict): El diccionario de respuestas ya formateado.
+            
+        Returns:
+            response: La respuesta de la API de Linkaform al crear el registro.
+        """
+        metadata = self.lkf_api.get_metadata(form_id=form_id)
+        
+        metadata.update({
+            "properties": {
+                "device_properties":{
+                    "System": "Script",
+                    "Module": module,
+                    "Process": process,
+                    "Action": action,
+                    "File": file
+                }
+            },
+        })
+        
+        metadata.update({'answers':answers})
+        response = self.lkf_api.post_forms_answers(metadata)
+        response = self.detail_response(response.get('status_code', 0))
+        return response
+
 if __name__ == "__main__":
     acceso_obj = Accesos(settings, sys_argv=sys.argv)
     acceso_obj.console_run()
     print('answers', simplejson.dumps(acceso_obj.answers, indent=3))
 
     data = acceso_obj.format_data_area(acceso_obj.answers)
+    if (data.get('option') == 'actualizar_foto_con_selección_de_nombre' or data.get('option') == 'actualización_de_qr') \
+        and data.get('create_area'):
+            acceso_obj.create_new_area(data)
+            data['area'] = data.get('nombre_nueva_area')
+        
     response = acceso_obj.update_area(data)
     if response:
         acceso_obj.answers[acceso_obj.f['status_details']] = response.get('message', 'No se pudo actualizar el área')
