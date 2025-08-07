72668436bbdb12c75f0eaf04b2e5f132b8c39513
paco soto <francisotomtz@gmail.com>
2025-08-07 15:08:48 -0600

    refactor: Se agrega validacion al hacer un check de ubicaciones, si existe un rondin en curso se comprueba si ya paso una hora del ultimo check de esa area, si si crea una bitacora nueva

--- accesos/items/scripts/Accesos/accesos_utils.py
+++ accesos/items/scripts/Accesos/accesos_utils.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
+from datetime import datetime, timedelta
 from linkaform_api import base
 from lkf_addons.addons.accesos.app import Accesos
+import pytz
 
 
 class Accesos( Accesos):
@@ -634,3 +635,66 @@ class Accesos( Accesos):
     #     #         "gefetes_pendientes": 0,
     #     #     }
     #     return res
+    
+    def get_rondines_by_status(self, status_list=['programado', 'en_proceso']):
+        query = [
+            {'$match': {
+                "deleted_at": {"$exists": False},
+                "form_id": self.BITACORA_RONDINES,
+                f"answers.{self.f['estatus_del_recorrido']}": {"$in": status_list},
+            }},
+            {'$project': {
+                '_id': 1,
+                'fecha_programacion': f"$answers.{self.f['fecha_programacion']}",
+                'answers': f"$answers"
+            }},
+        ]
+
+        rondines = self.format_cr(self.cr.aggregate(query))
+        return rondines
+
+    def close_rondines(self, list_of_rondines):
+        #- Expirados son lo que esta en status programados y que tienen mas de 24 de programdos
+        # - en progreso son lo que estan con status progreso y tienen mas de 1 hr de su ultimo check.
+        answers = {}
+        tz = pytz.timezone('America/Mexico_City')
+        ahora = datetime.now(tz)
+
+        rondines_expirados = []
+        rondines_en_proceso_vencidos = []
+
+        for rondin in list_of_rondines:
+            estatus = rondin.get('estatus_del_recorrido')
+            fecha_programacion_str = rondin.get('fecha_programacion')
+
+            if estatus == 'programado' and fecha_programacion_str:
+                fecha_programacion = tz.localize(datetime.strptime(fecha_programacion_str, '%Y-%m-%d %H:%M:%S'))
+                if ahora > fecha_programacion + timedelta(hours=24):
+                    rondines_expirados.append(rondin)
+            elif estatus == 'en_proceso':
+                areas = rondin.get('areas_del_rondin', [])
+                ultima_fecha = None
+                for area in areas:
+                    fecha_str = area.get('fecha_hora_inspeccion_area', '')
+                    if fecha_str:
+                        fecha = tz.localize(datetime.strptime(fecha_str, '%Y-%m-%d %H:%M:%S'))
+                        if not ultima_fecha or fecha > ultima_fecha:
+                            ultima_fecha = fecha
+                if ultima_fecha and ahora > ultima_fecha + timedelta(hours=1):
+                    rondines_en_proceso_vencidos.append(rondin)
+
+        rondines_ids = []
+        rondines_expirados = rondines_expirados + rondines_en_proceso_vencidos
+        for rondin in rondines_expirados:
+            rondines_ids.append(rondin.get('_id'))
+
+        answers[self.f['estatus_del_recorrido']] = 'cerrado'
+        answers[self.f['fecha_fin_rondin']] = ahora.strftime('%Y-%m-%d %H:%M:%S')
+
+        # print(stop)
+        if answers:
+            res = self.lkf_api.patch_multi_record(answers=answers, form_id=self.BITACORA_RONDINES, record_id=rondines_ids)
+            if res.get('status_code') == 201 or res.get('status_code') == 202:
+                return res
+            else: 
+                return res
