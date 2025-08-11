c7f225984f769f09b9a57794e7d31997f5763c8d
paco soto <francisotomtz@gmail.com>
2025-08-11 17:54:22 -0600

    refactor: Se agrega porcentaje de avance cuando se calculan los tiempos de traslado en la bitacora de rondines

--- accesos/items/scripts/Accesos/bitacora_rondines.py
+++ accesos/items/scripts/Accesos/bitacora_rondines.py
@@ -7,6 +7,14 @@ from accesos_utils import Accesos
 
 
 class Accesos(Accesos):
+    
+    def __init__(self, settings, folio_solicitud=None, sys_argv=None, use_api=False, **kwargs):
+        #--Variables
+        # Module Globals#
+        super().__init__(settings, sys_argv=sys_argv, use_api=use_api, **kwargs)
+        self.f.update({
+            'porcentaje_obtenido_bitacora': '689a7ecfbf2b4be31039388e'
+        })
 
     def calcluta_tiempo_traslados(self):
         fecha_inicio = self.answers[self.f['fecha_inicio_rondin']]
@@ -14,18 +22,21 @@ class Accesos(Accesos):
         areas_visitadas = self.answers[self.f['grupo_areas_visitadas']]
         fecha_inspeccion = 0
         duracion_total = 0
+        cantidad_de_inspecciones = 0
         areas_procesadas = False
         for area in areas_visitadas:
             fecha_inspeccion = area.get(self.f['fecha_inspeccion_area'])
             fecha_inspeccion = self.date_2_epoch(fecha_inspeccion)
             if not fecha_inspeccion:
                 continue
+            cantidad_de_inspecciones += 1
             areas_procesadas = True
             duracion = fecha_inspeccion - fecha_inicio
             area[self.f['duracion_traslado_area']] = round(duracion / 60,2)
             duracion_total = duracion
         if areas_procesadas:
             self.answers[self.f['duracion_rondin']] = round(duracion_total / 60,2)
+            self.answers[self.f['porcentaje_obtenido_bitacora']] = str(round((cantidad_de_inspecciones / len(areas_visitadas)) * 100, 2)) + '%'
         return True
 
 if __name__ == "__main__":
