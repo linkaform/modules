004e0ad209de8230a4ef8fc47fc23d82b112047f
paco soto <francisotomtz@gmail.com>
2025-08-22 08:23:01 -0600

    refactor: Se agregan datos a script que calcula tiempos en bitacora

--- accesos/items/scripts/Accesos/bitacora_rondines.py
+++ accesos/items/scripts/Accesos/bitacora_rondines.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+import datetime
+
 import sys, simplejson
 from linkaform_api import settings
 from account_settings import *
@@ -13,7 +15,8 @@ class Accesos(Accesos):
         # Module Globals#
         super().__init__(settings, sys_argv=sys_argv, use_api=use_api, **kwargs)
         self.f.update({
-            'porcentaje_obtenido_bitacora': '689a7ecfbf2b4be31039388e'
+            'porcentaje_obtenido_bitacora': '689a7ecfbf2b4be31039388e',
+            'cantidad_areas_inspeccionadas': '68a7b68a22ac030a67b7f8f8'
         })
 
     def calcluta_tiempo_traslados(self):
@@ -24,11 +27,13 @@ class Accesos(Accesos):
         duracion_total = 0
         cantidad_de_inspecciones = 0
         areas_procesadas = False
+        fecha_final = None
         for area in areas_visitadas:
             fecha_inspeccion = area.get(self.f['fecha_inspeccion_area'])
             fecha_inspeccion = self.date_2_epoch(fecha_inspeccion)
             if not fecha_inspeccion:
                 continue
+            fecha_final = fecha_inspeccion
             cantidad_de_inspecciones += 1
             areas_procesadas = True
             duracion = fecha_inspeccion - fecha_inicio
@@ -37,6 +42,10 @@ class Accesos(Accesos):
         if areas_procesadas:
             self.answers[self.f['duracion_rondin']] = round(duracion_total / 60,2)
             self.answers[self.f['porcentaje_obtenido_bitacora']] = str(round((cantidad_de_inspecciones / len(areas_visitadas)) * 100, 2)) + '%'
+            self.answers[self.f['cantidad_areas_inspeccionadas']] = str(cantidad_de_inspecciones) + '/' + str(len(areas_visitadas))
+        fecha_final_str = datetime.datetime.fromtimestamp(fecha_final).strftime('%Y-%m-%d %H:%M:%S') if fecha_final else ''
+        if self.answers.get(self.f['estatus_del_recorrido']) == 'realizado' and fecha_final_str:
+            self.answers[self.f['fecha_fin_rondin']] = fecha_final_str
         return True
 
 if __name__ == "__main__":
