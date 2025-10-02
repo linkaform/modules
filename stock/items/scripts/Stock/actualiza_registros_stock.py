29abb7ec08145140bf8ff7ea8b8b32ba689c992a
Roman Lezama <roman.lezama@linkaform.com>
2025-10-01 20:11:34 -0600

    Actualizar stock inventory

--- stock/items/scripts/Stock/actualiza_registros_stock.py
+++ stock/items/scripts/Stock/actualiza_registros_stock.py
@@ -6,7 +6,7 @@ from stock_utils import Stock
 from account_settings import *
 
 """
-python actualiza_registros_stock.py '{}' '{"jwt": "jwt eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNpZ2FfbWF0ZXJpYWxlcyIsInVzZXJfaWQiOjE3ODYwLCJwYXJlbnRfaWQiOjE3ODYwLCJpc19tb2JpbGUiOmZhbHNlLCJleHAiOjE3NTgzMDIwMzQsImRldmljZV9vcyI6IndlYiIsImVtYWlsIjoiYWRtaW5wY2xpbmtAb3BlcmFjaW9ucGNpLmNvbS5teCJ9.VXK04oP2KmgjLxMdLnh2iUVpl-VrGJREQF5lg1IZVwwWzvHDwsRxZw0fWow-Yq2hnOim1S9YV3Sj9Y7-KCA-7rdapcaIW-dQ5meliIvVh9YACIFucEYk7ZSoZpzARZXJvwpuiFYjKY82dq2ga8j98iI0-Fse5gqUzcllK4lPHweJ1Q-6k_vAzOQxLnNpUYNWxYzfMqR2OeL8odM77Y5-iV_C8ix5cgHYLoPgNSpL12ZBcr9m23PHXiMm1Qv0Oa-gzT35MxwlbcpiDdhljyYWGbuBpBjGZn8lYd9Oqi-ZEjQpwgQfacMyM-lPPBQMwCYpQi-m3pOvJFVZBeltIp4FI5B0HtGrk6EBDiZtBLYbj5IuIHAS2381hfrecDWmlI1nx749cqgP9KUtvcyu0PBL-8GihqH2EpQ-hFRZDjdpKjp6yS2iL3M1dzGwg43QqBYnSM9f8986L1zb3Tq0FywI5PN1fROQXm4pgEPP2MOnmYknNkCZ7Z8REWCFIVOwyZEG2J5CdCUpWEl7CBfjR94UXcRw1sdv4Blm9jLsa5o06K-1S11pAz2gOFpLiwSfFh13PWhtjtk69JTauBtRzLKI9K_nkMRNK9EmQeOg5yt6J8zLivtSGbRmc4tU5Bpb9VXroEMbsKzbY8Lu4rlFqfbnPmIyFJOuEu9R1fUF35Hoa-4"}'
+python actualiza_registros_stock.py '{}' '{"jwt": "jwt eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InNpZ2FfbWF0ZXJpYWxlcyIsInVzZXJfaWQiOjE3ODYwLCJwYXJlbnRfaWQiOjE3ODYwLCJpc19tb2JpbGUiOmZhbHNlLCJleHAiOjE3NTk5NjkxMjIsImRldmljZV9vcyI6IndlYiIsImVtYWlsIjoiYWRtaW5wY2xpbmtAb3BlcmFjaW9ucGNpLmNvbS5teCJ9.HFRkcflClT769b4mZqf2ACxZqJhAG6DM0Fu7-upOf6FYRyKdE-OydLBVx8uowkWEc3oMnH74iUQDsYCDIQFHQJqXw7yPKZQkld4TVOgrDe4hFNaj-YLs_hEvOd27ycTd7qOZOINTbjGUiwO9-BP5AMzTKDDOhM4bzWc-iHADUiLjxp5qBmdeOTn9kPneZyS4l7rlePuv9Xq0Z9OWNzE2E6x7K_1FEaqG4DFo8_3uAVcq6ZyecBuOTDIVK4Bevp5JZ-j_wYWiyxDfoFuJ_lTSxtaJV3AfdJyMPCRRuerz_xugFMYG1aMl1FochRqEnRLBDK9U21NLxfqD8I2Ygqzancvk8A2YNPuP_beUrnufDz_UPd7H1qQapn6rb4ZYhpi9PGDVCaUf-tJU-xcP-rWLmACmIjDAAvwqErNTT0el7Q9Yvn9Fh17bnAsQK5zI7RkpB-kHGZuGYbMmxX9VN5pkpBtEPJ2Iq8tnk_WOMY4g3N5TTZiAUNfmNt0fpjPaSnjHcpPcNwnBEGfZs0V-8MBmCZMdJibpVfJoV_aPVMYmcCFyVeCGzke-o0vvRQva-Bf6Dwr_I9DfbuwfXMcIQ7mLUqdnF_7hC8Y9KL2jhugmF6VIsfE1e2XFeqxQSrtbznc9BrP7kepH52xgyiB3TjQ4NtZpYNqdKHgF0_-PG_lJV7Y"}'
 """
 
 
@@ -18,18 +18,7 @@ class Stock(Stock):
         Se busca en la collecion de Stock el cache para hacer update
         """
         list_lotes = [
-            "ALCLFE0917E9", "ALCLFE091655", "ALCLFE08F23A", "ALCLFE0921C6", "ALCLFE08F9C0", "ALCLFE0913B8", "ALCLFE08F976", "ALCLFE08E939", 
-            "ALCLFE090A91", "ALCLFE08FDE5", "ALCLFE09122C", "ALCLFE08EB82", "ALCLFE08F1D1", "ALCLFE08E9DC", "ALCLFE090056", "ALCLFE08FABB", 
-            "ALCLFE08F3CE", "ALCLFE0905A0", "ALCLFE08EBE4", "ALCLFE08F4CB", "ALCLFE08F732", "ALCLFE08F4D8", "ALCLFE09092E", "ALCLFE08EEB3", 
-            "ALCLFE09160B", "ALCLFE09009C", "ALCLFE091A09", "ALCLFE0913FE", "ALCLFE091623", "ALCLFE08F317", "ALCLFE08FE1B", "ALCLFE0916FB", 
-            "ALCLFE0916F2", "ALCLFE0922C8", "ALCLFE08F94F", "ALCLFE092322", "ALCLFE0918EF", "ALCLFE08F1EA", "ALCLFE091E82", "ALCLFE09103B", 
-            "ALCLFE08F316", "ALCLFE08E984", "ALCLFE08F8F3", "ALCLFE08EEEE", "ALCLFE08EAE1", "ALCLFE08EB74", "ALCLFE08F420", "ALCLFE0911B0", 
-            "ALCLFE0915EF", "ALCLFE08F2D6", "ALCLFE08F74A", "ALCLFE0900A8", "ALCLFE08EBBC", "ALCLFE0922F7", "ALCLFE08F9EE", "ALCLFE0915F9", 
-            "ALCLFE08EF11", "ALCLFE08F8A8", "ALCLFE091647", "ALCLFE08FAE3", "ALCLFE09163F", "ALCLFE08FCC1", "ALCLFE092222", "ALCLFE08F3A1", 
-            "ALCLFE08F715", "ALCLFE0916F4", "ALCLFE08FAB8", "ALCLFE08EB79", "ALCLFE08F4C7", "ALCLFE09124F", "ALCLFE090092", "ALCLFE08ECBD", 
-            "ALCLFE08E9FB", "ALCLFE08F10B", "ALCLFE091881", "ALCLFE08FAC7", "ALCLFE08E8EC", "ALCLFE08F9FB", "ALCLFE091691", "ALCLFE08F71A", 
-            "ALCLFE08E860", "ALCLFE09005C", "ALCLFE0922C7", "ALCLFE08EAAA", "ALCLFE08FA45", "ALCLFE08EAA2", "ALCLFE08F8D4", "ALCLFE08F716", 
-            "ALCLFE08F495", "ALCLFE08FA67"
+            "ZTEG25036B6E", "ZTEG25036E18", 
         ]
         query = {
             'form_id':self.FORM_INVENTORY_ID, 
@@ -38,8 +27,10 @@ class Stock(Stock):
             # f"answers.{self.f['folio_recepcion']}":"REC-86",
             # # f"answers.{self.f['actuals']}":{'$exists':False},
 
-            "answers.6824e62e7c8af42c04a73d28.65ac6fbc070b93e656bd7fbe": "PCI Puebla",
-            f"answers.{self.f['lot_number']}": {'$in': list_lotes}
+            # "answers.6824e62e7c8af42c04a73d28.65ac6fbc070b93e656bd7fbe": "PCI Puebla",
+            "answers.6824e62e7c8af42c04a73d28.6442e4831198daf81456f274": "PRESEA",
+            f"answers.{self.f['lot_number']}": {'$in': list_lotes},
+            "answers.620ad6247a217dbcb888d175": "done" # "done" / "active"
 
             # "answers.644c36f1d20db114694a495a": "auditado",
             # 'answers.679bd3e1e213a8e4c124c05c': 'REC-71',
