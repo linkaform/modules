bace379a3843b7015431216af3e72a5e12bb1cb8
Linkaform Develop Team <backend@linkaform.com>
2025-10-09 12:27:33 -0600

    se cambio a que haga patch en vez de injeccion a base de datos

--- accesos/items/scripts/Accesos/rondines_cache.py
+++ accesos/items/scripts/Accesos/rondines_cache.py
@@ -510,15 +510,18 @@ class Accesos(Accesos):
             )
             
     def update_check_ubicacion(self):
-        response = self.cr.update_one({
-            'form_id': self.form_id,
-            '_id': ObjectId(self.record_id),
-        },{'$set': {
-            f'answers.{self.f["status_check_ubicacion"]}': 'Area no configurada'
-        }})
-        if response.matched_count > 0:
+        # response = self.cr.update_one({
+        #     'form_id': self.form_id,
+        #     '_id': ObjectId(self.record_id),
+        # },{'$set': {
+        #     f'answers.{self.f["status_check_ubicacion"]}': 'Area no configurada'
+        # }})
+        data = self.lkf_api.get_metadata(self.form_id)
+        data['answers'] = {self.f["status_check_ubicacion"]: 'Area no configurada'}
+        response = self.lkf_api.patch_record(data=data, record_id=self.record_id)
+        if response.get('status_code') in (200,201):
             print(f"===== log: Status actualizado. Area no configurada.")
-        return response.matched_count
+        return response.get('status_code')
 
 if __name__ == "__main__":
     script_obj = Accesos(settings, sys_argv=sys.argv)
