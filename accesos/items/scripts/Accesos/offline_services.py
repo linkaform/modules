9efb105c7d5d18a190e74cf3cbf06c5d9c3aa0d6
paco soto <francisotomtz@gmail.com>
2025-10-20 09:52:25 -0600

    feat: Se agrega script para los servicios offline para app movil de soter

---
+++ accesos/items/scripts/Accesos/offline_services.py
@@ -0,0 +1,98 @@
+# coding: utf-8
+import sys, simplejson, json
+from linkaform_api import settings
+from linkaform_api  import couch_util
+from account_settings import *
+
+from accesos_utils import Accesos
+from datetime import datetime
+
+class Accesos(Accesos):
+
+    def __init__(self, settings, folio_solicitud=None, sys_argv=None, use_api=False, **kwargs):
+        #--Variables
+        # Module Globals#
+        super().__init__(settings, sys_argv=sys_argv, use_api=use_api, **kwargs)
+        self.load(module='Location', **self.kwargs)
+        self.couch = couch_util.Couch_utils(self.settings)
+
+    def s(self):
+        breakpoint()
+        
+    def get_user_catalogs(self):
+        soter_catalogs = [
+            self.LISTA_INCIDENCIAS_CAT_ID,
+            self.SUB_CATEGORIAS_INCIDENCIAS_ID,
+            self.CATEGORIAS_INCIDENCIAS_ID,
+            self.AREAS_DE_LAS_UBICACIONES_CAT_ID,
+            self.UBICACIONES_CAT_ID,
+            self.CONFIGURACION_RECORRIDOS_ID,
+            self.USUARIOS_ID,
+            self.CONF_AREA_EMPLEADOS_CAT_ID,
+            self.TIPO_DE_EQUIPO_ID,
+            self.LISTA_FALLAS_CAT_ID,
+            self.CONF_AREA_EMPLEADOS_AP_CAT_ID,
+            self.VISITA_AUTORIZADA_CAT_ID,
+            self.ESTADO_ID,
+            self.PROVEEDORES_CAT_ID,
+            self.LOCKERS_CAT_ID,
+            self.TIPO_ARTICULOS_PERDIDOS_CAT_ID,
+        ]
+        dbs = {}
+        try:
+            for catalog_id in soter_catalogs:
+                item = {}
+                version = "00.00"
+                info_catalog = self.lkf_api.get_catalog_id_fields(catalog_id)
+                catalog_name = info_catalog.get('catalog', {}).get('name', '').lower().replace(' ', '_')
+                catalog_fields = info_catalog.get('catalog', {}).get('fields', [])
+                catalog_updated_at = info_catalog.get('catalog', {}).get('updated_at', '')
+                
+                field_items = {}
+                for field in catalog_fields:
+                    if not field.get('field_type') in ['catalog']:
+                        field_items.update({
+                            field.get('field_id'): field.get('label')
+                        })
+                
+                if catalog_updated_at:
+                    date_part = catalog_updated_at[:10]
+                    dt = datetime.strptime(date_part, '%Y-%m-%d')
+                    version = f"{dt.year % 100:02d}.{dt.month:02d}"
+                
+                item = {
+                    'db_name': f'catalog_records_{info_catalog.get("catalog", {}).get("catalog_id", 0)}',
+                    'field_name': field_items,
+                    'version': version,
+                    'host': '',
+                    'filter': ''
+                }
+                dbs[catalog_name] = item
+        except Exception as e:
+            return {'status_code': 400, 'msg': 'error', 'data': str(e)}
+        return {'status_code': 200, 'msg': 'success', 'data': dbs}
+    
+    def sync_to_lkf(self):
+        cr_db = self.lkf_api.couch.set_db('incidencias_10')
+        mango_query = {
+            "selector": {},
+            "fields": ["_id", "_rev"]
+        }
+        records = cr_db.find(mango_query)
+        for record in records:
+            print('---------------', record)
+        breakpoint()
+            
+
+if __name__ == "__main__":
+    acceso_obj = Accesos(settings, sys_argv=sys.argv)
+    acceso_obj.console_run()
+    data = acceso_obj.data.get('data', {})
+    option = data.get("option", 'sync_to_lkf')
+
+    if option == 'get_user_catalogs':
+        response = acceso_obj.get_user_catalogs()
+    elif option == 'sync_to_lkf':
+        response = acceso_obj.sync_to_lkf()
+
+    sys.stdout.write(simplejson.dumps(response))
