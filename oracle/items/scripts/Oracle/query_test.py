31b4e706dfa334e7bced5b370e118a3412938975
Linkaform Develop Team <backend@linkaform.com>
2025-11-21 13:52:10 -0600

    monitore 72 hrs de variables

--- oracle/items/scripts/Oracle/query_test.py
+++ oracle/items/scripts/Oracle/query_test.py
@@ -14,29 +14,55 @@ class Oracle(Oracle):
         #use self.lkm.catalog_id() to get catalog id
         self.name =  __class__.__name__
         self.settings = settings
+        self.QUERY_FORM_ID = 143654
+        self.m = {
+            'fecha':'691f4838444e45f9f6e2e648',
+            'variable':'691f4838444e45f9f6e2e649',
+            'resultado':'691f4838444e45f9f6e2e64a'
+        }
 
+    def get_form_model(self):
+        """
+        Obtiene el modelo de la forma
+        """
+        metadata = self.lkf_api.get_metadata(self.QUERY_FORM_ID)
+        properties = {
+                "device_properties":{
+                    "system": "Script",
+                    "process": "Sync Catalogs", 
+                    "accion": 'sync_catalogs', 
+                    "archive": "oracle/sync_catalogs.py",
+                },
+            }
+        metadata.update({
+            'properties': properties,
+            'answers': {}
+            },
+        )
+        return metadata
 
-
-    def run_query(self):
+    def run_query(self, variable):
         db_name = 'PRODUCCION.VW_LinkAForm_Hora'
         query = "SELECT * FROM PRODUCCION.VW_LinkAForm_Hora "
         query += " WHERE "
-        query += " VARIA='LECBRIXJCLARO'"
+        query += " VARIA='{}'".format(variable)
         query += """ AND TRUNC(DATA) + (TO_DATE(HORA, 'HH24:MI') - TRUNC(TO_DATE(HORA, 'HH24:MI')))
-                >= TO_DATE('2025-11-19 00:00', 'YYYY-MM-DD HH24:MI')
+                >= TO_DATE('2025-11-20 00:00', 'YYYY-MM-DD HH24:MI')
         """
-        print('query=',query)
-        for x in range(3):
+        form_model = self.get_form_model()
+        answers = {}
+        for x in range(144):
             header, data = self.query_view(db_name, query=query, date_format=True)
-            # print('data=',data)
-            print('date', datetime.datetime.now())
             res = ''
             for d in data:
-                res +='row = DATA: {} , Varia: {}, Valor: {}, Hora: {}'.format(d['DATA'],d['VARIA'],d['VALOR'],d['HORA'])
+                res +='row = DATA: {} , Hora: {}, Valor: {}'.format(d['DATA'],d['HORA'],d['VALOR'])
                 res += "\n"
-            print('data=',res)
-            print('='*70)
-            time.sleep(1)
+            answers[self.m['fecha']] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            answers[self.m['variable']] = variable
+            answers[self.m['resultado']] = res
+            form_model['answers'] = answers
+            res = self.lkf_api.post_forms_answers_list(form_model)
+            time.sleep(1800)
 
 
 
@@ -44,12 +70,8 @@ if __name__ == "__main__":
     """
     Formato last_update_date = '2025-08-25 15:00'
     """
-    oo  = settings.config['ORACLE_HOST']
     module_obj = Oracle(settings, sys_argv=sys.argv)
     module_obj.console_run()
     module_obj.db_updated_at = time.time()
-    #gg = module_obj.search_views()
-    # print('account settings', module_obj.settings.config)
-    data = module_obj.data.get('data',{})
-    option = data.get("option",'read')
-    module_obj.run_query()
+    variable = module_obj.data.get("variable",)
+    module_obj.run_query(variable)
