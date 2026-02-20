b61ee22c26c8ddd24305421105278c076ddf4a1a
paco soto <francisotomtz@gmail.com>
2026-02-20 13:44:52 -0600

    feat: Se cambia la generacion del pdf a img en pase de acceso

--- accesos/items/scripts/Accesos/accesos_utils.py
+++ accesos/items/scripts/Accesos/accesos_utils.py
@@ -4031,6 +4031,20 @@ class Accesos( Accesos):
                 self.LKFException({'title': 'Error', 'msg': 'Hubo un error al actualizar los registros.'})
         return format_data
 
+    def get_pass_img(self, qr_code):
+        answers = {}
+        pdf_to_img = self.update_pass_img(qr_code)
+        if pdf_to_img:
+            answers.update({self.pase_entrada_fields['pdf_to_img']: pdf_to_img})
+            response = self.lkf_api.patch_multi_record( answers = answers, form_id=self.PASE_ENTRADA, record_id=[qr_code])
+            if response.get('status_code') in [200, 201, 202]:
+                url = self.unlist(pdf_to_img).get('file_url') if len(pdf_to_img) > 0 else ''
+                return url
+            else:
+                print('=============', response)
+                self.LKFException({'title': 'Error', 'msg': 'Hubo un error al actualizar los registros.'})
+        return False
+
     def update_pass(self, access_pass,folio=None):
         pass_selected= self.get_detail_access_pass(qr_code=folio, get_answers=True)
         qr_code= folio
@@ -4103,10 +4117,6 @@ class Accesos( Accesos):
 
         employee = getattr(self,'employee',self.get_employee_data(email=self.user.get('email'), get_one=True))
         if answers:
-            pdf_to_img = self.update_pass_img(qr_code)
-            if pdf_to_img:
-                answers.update({self.pase_entrada_fields['pdf_to_img']: pdf_to_img})
-
             new_answers = deepcopy(pass_selected['answers'])
             new_answers.update(answers)
             status = self.access_pass_set_status(new_answers)
@@ -4126,7 +4136,6 @@ class Accesos( Accesos):
                 res['json'].update({'fecha_hasta':pass_selected.get('fecha_de_caducidad')})
                 res['json'].update({'asunto':pass_selected.get('tema_cita')})
                 res['json'].update({'descripcion':pass_selected.get('descripcion')})
-                res['json'].update({'pdf_to_img': pdf_to_img if pdf_to_img else pass_selected.get('pdf_to_img')})
                 res['json'].update({'pdf': pdf})
                 return res
             else: 
