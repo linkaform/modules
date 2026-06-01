1b9a75bbbbdada5ff0f37fc0580f0b8c9968ec34
Maricruz Alvarado <maricruzalvaradoc@outlook.com>
2026-06-01 12:25:14 -0600

    Fix: se agrega proveedores a los filtros de paqueteria

--- accesos/items/scripts/Accesos/filters.py
+++ accesos/items/scripts/Accesos/filters.py
@@ -174,6 +174,12 @@ class Accesos(Accesos):
             "field": f"answers.{self.notes_fields['note_status']}"
         }
     @get_mongo_distinct_list
+    def get_proveedores(self):
+        return {
+            "form_id": self.PAQUETERIA,
+            "field": f"answers.{self.paquetes_fields['proveedor_cat']}.{self.paquetes_fields['proveedor']}"
+        }
+    @get_mongo_distinct_list
     def get_pases_status(self):
         return {
             "form_id": self.PASE_ENTRADA,
@@ -458,6 +464,7 @@ class Accesos(Accesos):
         reportado_por = self.get_employees_names()
         areas = self.get_areas()
         lockers = self.get_lockers()
+        proveedor = self.get_proveedores()
         return [
             {
                 "defaultDisplayOpen": True,
@@ -479,6 +486,13 @@ class Accesos(Accesos):
                 "label": "Locker ",
                 "type": "multiselect",
                 "options": [{"label": i, "value": i} for i in lockers]
+            },
+            {
+                "defaultDisplayOpen": False,
+                "key": "proveedor",
+                "label": "Proveedor",
+                "type": "multiselect",
+                "options": [{"label": i, "value": i} for i in proveedor]
             },
             {
                 "defaultDisplayOpen": False,
