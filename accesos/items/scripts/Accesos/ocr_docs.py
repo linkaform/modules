ed4e3f71a0d9a7545928cc292a82d834d17803d6
Roman Lezama <roman.lezama@linkaform.com>
2026-09-12 22:39:30 -0600

    al leer la imagen se valida si el modelo es HUAWEI se formatean las series

--- accesos/items/scripts/Accesos/ocr_docs.py
+++ accesos/items/scripts/Accesos/ocr_docs.py
@@ -682,6 +682,12 @@ class Accesos(Accesos):
         )
         datos['confirmedAt'] = fecha_monterrey.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]
 
+        modelo_series = datos.get('modelo') or ''
+        if modelo_series.lower() == 'huawei':
+            for serial in datos.get('serials', []):
+                sn = serial.get('sn', '')
+                serial['sn'] = f"HWTC{sn[8:]}" if sn else None
+
         if errores:
             return {
                 'status_code': 206,
